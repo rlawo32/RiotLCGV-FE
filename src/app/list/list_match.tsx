5debafa724378fbbb959b8dc08c1b4ac2ea82710
rlawo <rlawo32@naver.com>
2024-12-17 01:16:46 +0900

    [RiotLCGV][FE] <View> ListMatch_작업_20241217 (2)

---
+++ src/app/list/list_match.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import { useEffect, useRef, useState } from "react";
+import supabase from "@/app/supabase";
+
+// import * as Styled from "./page.style";
+
+const ListMatch = () => {
+    const client:any = supabase();
+
+    const [locationData, setLocationData] = useState<string>("");
+    
+    useEffect(() => {
+        const lcgMatchMain = async():Promise<any> => {
+            let {data:lcg_match_main, error} = await client
+                .from("lcg_match_main")
+                .select("*")
+                .eq("lcg_game_id", 7389173588)
+
+            return lcg_match_main;
+        }
+
+        lcgMatchMain().then((data) => {
+            console.log(data);
+        });
+    }, [])
+
+    return (
+        <div>
+            <h1>HELLO</h1>
+        </div>
+    )
+}
+
+export default ListMatch;
