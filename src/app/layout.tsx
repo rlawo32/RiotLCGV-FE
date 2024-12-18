8c59f4d8d6137cfe649c0a318abe3942ce543be9
rlawo <rlawo32@naver.com>
2024-12-18 23:29:01 +0900

    [RiotLCGV][FE] <COMMON> Styled_components_추가_20241218 (3)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+import StyledComponentsRegistry from "./StyledComponentsRegistry";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -25,7 +27,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+        <StyledComponentsRegistry>
+          {children}
+        </StyledComponentsRegistry>
       </body>
     </html>
   );
