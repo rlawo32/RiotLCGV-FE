b07d3936c1259efe6e77b5822c9a587256bf57c9
rlawo <rlawo32@naver.com>
2024-12-22 01:38:51 +0900

    [RiotLCGV][FE] <View> MatchHistory_작업_20241222 (2)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,13 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 import ListMatch from "./component/match_list";
+import ListHistory from "./component/match_history";
+
 
 export default function Home() {
   return (
     <div>
-      <ListMatch />
+      <ListHistory />
     </div>
   );
 }
