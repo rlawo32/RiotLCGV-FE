c6c1571303c321b19d76e5b85d20304b2fe7cb5a
rlawo <rlawo32@naver.com>
2024-12-23 20:38:03 +0900

    [RiotLCGV][FE] <View> GraphTest_작업_20241223 (2)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,13 @@ import styles from "./page.module.css";
 
 import ListMatch from "./component/match_list";
 import ListHistory from "./component/match_history";
+import ChartTest  from "./component/chart_test";
 
 
 export default function Home() {
   return (
     <div>
-      <ListHistory />
+      <ChartTest />
     </div>
   );
 }
