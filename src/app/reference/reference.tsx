9fcb88ac337c63d9039bbedaf00211069b6203a2
sungjae <92013056+rlawo32@users.noreply.github.com>
2024-12-26 14:43:02 +0900

    Create reference.tsx

---
+++ src/app/reference/reference.tsx
@@ -0,0 +1,64 @@
+'use client';
+
+import { useRef } from "react";
+import styled from "styled-components";
+
+const Reference = styled('div')`
+    .test_btn {
+      height: 50px;
+      width: 100px;
+      text-align: center;
+      line-height: 50px;
+      color: black;
+      background-color: white;
+      cursor: pointer;
+    }
+    
+    .test_box {
+      height: 0;
+      overflow: hidden;
+      transition: all 0.4s ease;
+    }
+    
+    .test_box.sizeUp {
+      height: 160px;
+      background-color: red;
+    }
+`;
+
+const Reference = () => {
+  const testRef:any = useRef<any>([]);
+  
+  const testClick = (flag:boolean, target:number) => {
+
+        if(!testRef.current[target].className.includes('sizeUp')) {
+            testRef.current[target].className += ' sizeUp';
+        } else {            
+            testRef.current[target].className = testRef.current[target].className.replace(' sizeUp', '');
+        }
+
+        for(let i:number=0; i<testRef.current.length; i++) {
+            if(i !== target) {
+                testRef.current[i].className = testRef.current[i].className.replace(' sizeUp', '');
+            }
+        }
+        setTestYn(!flag);
+  }
+
+  return (
+    <Reference>
+      <div className="test_btn" onClick={() => testClick(testYn, 0)}> 
+        TEST1
+      </div>
+      <div className="test_box" ref={(li:any) => (testRef.current[0] = li)}>
+        HELLO1
+      </div>   
+      <div className="test_btn" onClick={() => testClick(testYn, 1)}>
+        TEST2
+      </div>
+      <div className="test_box" ref={(li:any) => (testRef.current[1] = li)}>
+        HELLO2
+      </div>
+    </Reference>
+  )
+}
