2cb72ef038e72b0951bff93c564c6435c92a2b53
rlawo <rlawo32@naver.com>
2024-12-29 02:44:19 +0900

    [RiotLCGV][FE] <View> DamageGraph_작업_20241229 (5)

--- src/app/component/damage_graph.tsx
+++ src/app/component/damage_graph.tsx
@@ -3,17 +3,21 @@
 import styled from "styled-components";
 
 const GraphTotal = styled('div')`
-    height: 10px;
+    height: 13px;
     width: 50px;
     margin: 5px auto;
+    padding: 1px;
     background-color: #31313C;
-    border: 1px solid white;
+    border: 1px solid #3d96ff;
+    border-radius: 5px;
 `;
 
 const GraphTarget = styled('div')<{$data:number, $color:string}>`
-    height: 8px;
+    height: 9px;
     width: ${({$data}) => $data + "%"};
     background-color: ${({$color}) => $color};
+    border: 1px solid ${({$color}) => $color};
+    border-radius: 5px;
 `;
 
 const DamageGraph = (props : {standard:number, target:number, flag:string}) => {
