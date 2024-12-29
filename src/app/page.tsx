f4864ae0359f099bc409f41946af71e6d91a4ba7
rlawo <rlawo32@naver.com>
2024-12-30 02:01:54 +0900

    [RiotLCGV][FE] <Build> Never Use_오류_수정_20241230 (1)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,4 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-
 import ListMatch from "./component/match_list";
-import ListHistory from "./component/match_history";
 
 export default function Home() {
   return (
