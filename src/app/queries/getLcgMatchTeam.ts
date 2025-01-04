cc7edad040ec5d250ee62deb59e057b31e72a49e
rlawo <rlawo32@naver.com>
2025-01-05 04:06:50 +0900

    [RiotLCGV][FE] <SUPABASE> ReactQuery_세팅_20250105 (8)

---
+++ src/app/queries/getLcgMatchTeam.ts
@@ -0,0 +1,8 @@
+import { TypedSupabaseClient } from "../supabase" 
+
+export function getLcgMatchTeam(client:TypedSupabaseClient, gameId:number) {
+  return client
+    .from("lcg_match_team")
+    .select("*")
+    .eq("lcg_game_id", gameId)
+}
