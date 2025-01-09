80c998c1d6bc30f03424c88cb6e209ef09021386
rlawo <rlawo32@naver.com>
2025-01-10 01:00:06 +0900

    [RiotLCGV][FE] <Icon> 구조_수정_20250110

--- src/app/icons/TurretIcon.tsx
+++ src/app/icons/TurretIcon.tsx
@@ -2,17 +2,15 @@
 const TurretIcon = () => {
 
     return (
-        <>
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="white">
-                <path fillRule="evenodd" clipRule="evenodd" d="M4 4L8 0L11.9992 4L10.9982 5.0012L11 5H14L8 11L2 5H5L4 
-                4ZM6.4 3.99963L8 2.4L9.6 3.99963L8 5.6L6.4 3.99963ZM8 12L12 8L10 16H6L4 8L8 12Z" fill="white"></path>
-                <mask id="mask0_5762_288549" style={{maskType:"luminance"}} maskUnits="userSpaceOnUse" x="2" y="0" width="12" height="16" fill="white">
-                    <path fillRule="evenodd" clipRule="evenodd" d="M4 4L8 0L11.9992 4L10.9982 5.0012L11 5H14L8 11L2 
-                    5H5L4 4ZM6.4 3.99963L8 2.4L9.6 3.99963L8 5.6L6.4 3.99963ZM8 12L12 8L10 16H6L4 8L8 12Z" fill="white"></path>
-                </mask>
-                <g mask="url(#mask0_5762_288549)" fill="white"></g>
-            </svg>
-        </>
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="white">
+            <path fillRule="evenodd" clipRule="evenodd" d="M4 4L8 0L11.9992 4L10.9982 5.0012L11 5H14L8 11L2 5H5L4 
+            4ZM6.4 3.99963L8 2.4L9.6 3.99963L8 5.6L6.4 3.99963ZM8 12L12 8L10 16H6L4 8L8 12Z" fill="white"></path>
+            <mask id="mask0_5762_288549" style={{maskType:"luminance"}} maskUnits="userSpaceOnUse" x="2" y="0" width="12" height="16" fill="white">
+                <path fillRule="evenodd" clipRule="evenodd" d="M4 4L8 0L11.9992 4L10.9982 5.0012L11 5H14L8 11L2 
+                5H5L4 4ZM6.4 3.99963L8 2.4L9.6 3.99963L8 5.6L6.4 3.99963ZM8 12L12 8L10 16H6L4 8L8 12Z" fill="white"></path>
+            </mask>
+            <g mask="url(#mask0_5762_288549)" fill="white"></g>
+        </svg>
     )
 }
 
