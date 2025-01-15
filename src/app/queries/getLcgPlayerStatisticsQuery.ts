6b8881acd5550b38392c7a510e8c369aad4b18e0
rlawo <rlawo32@naver.com>
2025-01-16 02:09:56 +0900

    [RiotLCGV][FE] <SUPABASE> Player_Statistics_Query_작업_20250116 (2)

--- src/app/queries/getLcgPlayerStatisticsQuery.ts
+++ src/app/queries/getLcgPlayerStatisticsQuery.ts
@@ -6,100 +6,11 @@ export function getLcgPlayerStatisticsQuery(client:TypedSupabaseClient) {
     .select("*")
 }
 
-export function getLcgOneGameBest3KillQuery(client:TypedSupabaseClient) {
-  return client
-    .from("lcg_match_main")
-    .select("lcg_summoner_name, lcg_summoner_tag, lcg_kill_count")
-    .order("lcg_kill_count", { ascending: false })
-    .limit(3)
+export function getLcgOneGameBest3GoldQuery(client:TypedSupabaseClient) {
+  return client.rpc('onegame_best3_gold_rank')
 }
 
-export function getLcgOneGameBest3DeathQuery(client:TypedSupabaseClient) {
-  return client
-    .from("lcg_match_main")
-    .select("lcg_summoner_name, lcg_summoner_tag, lcg_death_count")
-    .order("lcg_death_count", { ascending: false })
-    .limit(3)
+export function getLcgBest3GoldQuery(client:TypedSupabaseClient) {
+  return client.rpc('best3_gold_rank')
 }
 
-export function getLcgOneGameBest3AssistQuery(client:TypedSupabaseClient) {
-  return client
-    .from("lcg_match_main")
-    .select("lcg_summoner_name, lcg_summoner_tag, lcg_assist_count")
-    .order("lcg_assist_count", { ascending: false })
-    .limit(3)
-}
-
-export function getLcgBest3KillQuery(client:TypedSupabaseClient) {
-  return client
-    .from("lcg_player_statistics")
-    .select("lcg_player, lcg_nickname, lcg_count_kill")
-    .order("lcg_count_kill", { ascending: false })
-    .limit(3)
-}
-
-export function getLcgBest3DeathQuery(client:TypedSupabaseClient) {
-  return client
-    .from("lcg_player_statistics")
-    .select("lcg_player, lcg_nickname, lcg_count_death")
-    .order("lcg_count_death", { ascending: false })
-    .limit(3)
-}
-
-export function getLcgBest3AssistQuery(client:TypedSupabaseClient) {
-  return client
-    .from("lcg_player_statistics")
-    .select("lcg_player, lcg_nickname, lcg_count_assist")
-    .order("lcg_count_assist", { ascending: false })
-    .limit(3)
-}
-
-export function getLcgBest3MinionQuery(client:TypedSupabaseClient) {
-  return client
-    .from("lcg_player_statistics")
-    .select("lcg_player, lcg_nickname, lcg_count_minion")
-    .order("lcg_count_minion", { ascending: false })
-    .limit(3)
-}
-
-export function getLcgBest3JungleQuery(client:TypedSupabaseClient) {
-  return client
-    .from("lcg_player_statistics")
-    .select("lcg_player, lcg_nickname, lcg_count_jungle")
-    .order("lcg_count_jungle", { ascending: false })
-    .limit(3)
-}
-
-export function getLcgBest3CrowdQuery(client:TypedSupabaseClient) {
-  return client
-    .from("lcg_player_statistics")
-    .select("lcg_player, lcg_nickname, lcg_count_crowd_time")
-    .order("lcg_count_crowd_time", { ascending: false })
-    .limit(3)
-}
-
-export function getLcgBest3VisionScoreQuery(client:TypedSupabaseClient) {
-  return client
-    .from("lcg_player_statistics")
-    .select("lcg_player, lcg_nickname, lcg_count_vision_score")
-    .order("lcg_count_vision_score", { ascending: false })
-    .limit(3)
-}
-
-export function getLcgBest3WardKillQuery(client:TypedSupabaseClient) {
-  return client
-    .from("lcg_player_statistics")
-    .select("lcg_player, lcg_nickname, lcg_count_ward_kill")
-    .order("lcg_count_ward_kill", { ascending: false })
-    .limit(3)
-}
-
-export function getLcgBest3VisionWardQuery(client:TypedSupabaseClient) {
-  return client
-    .from("lcg_player_statistics")
-    .select("lcg_player, lcg_nickname, lcg_count_vision_ward")
-    .order("lcg_count_vision_ward", { ascending: false })
-    .limit(3)
-}
-
-// 승률, 정글 오브젝트, 멀티킬 횟수
