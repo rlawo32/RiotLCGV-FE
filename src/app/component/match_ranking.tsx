995e8c1d9f176bc8c13954bc9ebdd8554bc2f1ff
rlawo <rlawo32@naver.com>
2025-01-16 02:09:14 +0900

    [RiotLCGV][FE] <View> Ranking_초기작업_20250116 (1)

--- src/app/component/match_ranking.tsx
+++ src/app/component/match_ranking.tsx
@@ -2,27 +2,47 @@
 
 import * as Style from "./match_ranking.style"
 
+import { useQuery } from "@supabase-cache-helpers/postgrest-react-query";
+import { getLcgBest3GoldQuery } from "../queries/getLcgPlayerStatisticsQuery";
+import useSupabaseBrowser from "../supabase-browser";
+import Image from "next/image";
+
 const MatchRanking = () => {
+    const supabase = useSupabaseBrowser();
+    
+    const { data: best3gold } = useQuery(getLcgBest3GoldQuery(supabase));
+
+    if(best3gold !== null && best3gold !== undefined) {
+        
+    }
 
     return (
         <Style.MatchRanking>
-            <div className="ranking_box">
-                <div className="ranking_item ranking-2">
-                    <div className="ranker_img" />
-                    <div className="ranker_name">JSON</div>
-                    <div className="ranker_detail">총 딜량 100,000</div>
+            {best3gold ? 
+                <div className="ranking_box">
+                    <div className="ranking_item ranking-2">
+                        <div className="ranker_img" />
+                        <Image src={"/img/border_silver_image.png"} alt={"ranking_border"} 
+                        height={170} width={170} className="ranker_img" />
+                        <div className="ranker_name">{best3gold[1].lcg_nickname.split('#')[0]}</div>
+                        <div className="ranker_detail">총 골드 {best3gold[1].lcg_count_gold.toLocaleString()}</div>
+                    </div>
+                    <div className="ranking_item ranking_1">
+                        <Image src={"/img/border_gold_image.png"} alt={"ranking_border"} 
+                        height={170} width={170} className="ranker_img" />
+                        <div className="ranker_name">{best3gold[0].lcg_nickname.split('#')[0]}</div>
+                        <div className="ranker_detail">총 골드 {best3gold[0].lcg_count_gold.toLocaleString()}</div>
+                    </div>
+                    <div className="ranking_item ranking_3">
+                        <Image src={"/img/border_bronze_image.png"} alt={"ranking_border"} 
+                        height={170} width={170} className="ranker_img" />
+                        <div className="ranker_name">{best3gold[2].lcg_nickname.split('#')[0]}</div>
+                        <div className="ranker_detail">총 골드 {best3gold[2].lcg_count_gold.toLocaleString()}</div>
+                    </div>
                 </div>
-                <div className="ranking_item ranking_1">
-                    <div className="ranker_img" />
-                    <div className="ranker_name">JAVA</div>
-                    <div className="ranker_detail">총 딜량 200,000</div>
-                </div>
-                <div className="ranking_item ranking_3">
-                    <div className="ranker_img" />
-                    <div className="ranker_name">PHP</div>
-                    <div className="ranker_detail">총 딜량 50,000</div>
-                </div>
-            </div>
+            
+            : <></>}
+            
         </Style.MatchRanking>
     )
 }
