8f102c35e5a838cfa1152d795a5e5f262823f695
rlawo <rlawo32@naver.com>
2025-01-18 19:36:49 +0900

    [RiotLCGV][FE] <SUPABASE> Player_Statistics_Query_작업_20250118 (3)

--- src/app/queries/getLcgPlayerStatisticsQuery.ts
+++ src/app/queries/getLcgPlayerStatisticsQuery.ts
@@ -6,11 +6,15 @@ export function getLcgPlayerStatisticsQuery(client:TypedSupabaseClient) {
     .select("*")
 }
 
+export function getLcgAllWinningRateQuery(client:TypedSupabaseClient) {
+  return client.rpc('all_winningrate_rank')
+}
+
 export function getLcgOneGameBest3GoldQuery(client:TypedSupabaseClient) {
   return client.rpc('onegame_best3_gold_rank')
 }
 
-export function getLcgBest3GoldQuery(client:TypedSupabaseClient) {
-  return client.rpc('best3_gold_rank')
+export function getLcgAllGoldQuery(client:TypedSupabaseClient) {
+  return client.rpc('all_gold_rank')
 }
 
