d51d7cad53c43ea76587fd09f88b50ca857e7101
rlawo <rlawo32@naver.com>
2025-01-18 21:52:39 +0900

    [RiotLCGV][FE] <View> Ranking_데스_View 작업_20250118 (2)

--- src/app/ranking/rankingView_death.tsx
+++ src/app/ranking/rankingView_death.tsx
@@ -12,13 +12,6 @@ const RankingViewDeath = (props : {data:{
             rank: number
         }[]}) => {
 
-    const testArr:{lcg_nickname: string, lcg_count_death: number, rank: number}[] = [
-        {lcg_nickname:'hello1', lcg_count_death:1, rank:1},
-        {lcg_nickname:'hello1', lcg_count_death:1, rank:2},
-        {lcg_nickname:'hello1', lcg_count_death:1, rank:3},
-        {lcg_nickname:'hello1', lcg_count_death:1, rank:3},
-    ];
-
     return (
         <>
             {/* {props.data.map((rankingData) => {
@@ -27,46 +20,46 @@ const RankingViewDeath = (props : {data:{
                 )
             })} */}
             <div className="ranking_top">    
-                <Style.RankingBox $ea={testArr.filter((item) => item.rank === 2).length}>
-                    {testArr.filter((highRanking) => highRanking.rank === 2).map((item, idx, arr) => {
+                <Style.RankingBox $ea={props.data.filter((item) => item.rank === 2).length}>
+                    {props.data.filter((highRanking) => highRanking.rank === 2).map((item, idx, arr) => {
                         return (
                             <Style.RankingItem $ea={arr.length} $rank={2} key={"rank2_" + idx}>
                                 <Image src={"/img/border_silver_image.png"} alt={"ranking_border"} 
                                 height={duplicationRank(arr.length)} width={duplicationRank(arr.length)} className="ranker_img" />
                                 <Style.RankerContent $ea={arr.length}>
                                     <div className="ranker_name">{item.lcg_nickname.split('#')[0]}</div>
-                                    <div className="ranker_title">총 {item.lcg_count_death} 데스</div>
-                                    <div className="item_detail"></div>
+                                    <div className="ranker_title"></div>
+                                    <div className="ranker_detail">총 {item.lcg_count_death} 데스</div>
                                 </Style.RankerContent>
                             </Style.RankingItem>
                         )
                     })}
                 </Style.RankingBox>
-                <Style.RankingBox $ea={testArr.filter((item) => item.rank === 1).length}>
-                    {testArr.filter((highRanking) => highRanking.rank === 1).map((item, idx, arr) => {
+                <Style.RankingBox $ea={props.data.filter((item) => item.rank === 1).length}>
+                    {props.data.filter((highRanking) => highRanking.rank === 1).map((item, idx, arr) => {
                         return (
                             <Style.RankingItem $ea={arr.length} $rank={1} key={"rank1_" + idx}>
                                 <Image src={"/img/border_gold_image.png"} alt={"ranking_border"} 
                                 height={duplicationRank(arr.length)} width={duplicationRank(arr.length)} className="ranker_img" />
                                 <Style.RankerContent $ea={arr.length}>
                                     <div className="ranker_name">{item.lcg_nickname.split('#')[0]}</div>
-                                    <div className="ranker_title">총 {item.lcg_count_death} 데스</div>
-                                    <div className="item_detail"></div>
+                                    <div className="ranker_title"></div>
+                                    <div className="ranker_detail">총 {item.lcg_count_death} 데스</div>
                                 </Style.RankerContent>
                             </Style.RankingItem>
                         )
                     })}
                 </Style.RankingBox>
-                <Style.RankingBox $ea={testArr.filter((item) => item.rank === 3).length}>
-                    {testArr.filter((highRanking) => highRanking.rank === 3).map((item, idx, arr) => {
+                <Style.RankingBox $ea={props.data.filter((item) => item.rank === 3).length}>
+                    {props.data.filter((highRanking) => highRanking.rank === 3).map((item, idx, arr) => {
                         return (
                             <Style.RankingItem $ea={arr.length} $rank={3} key={"rank3_" + idx}>
                                 <Image src={"/img/border_bronze_image.png"} alt={"ranking_border"} 
                                 height={duplicationRank(arr.length)} width={duplicationRank(arr.length)} className="ranker_img" />
                                 <Style.RankerContent $ea={arr.length}>
                                     <div className="ranker_name">{item.lcg_nickname.split('#')[0]}</div>
-                                    <div className="ranker_title">총 {item.lcg_count_death} 데스</div>
-                                    <div className="item_detail"></div>
+                                    <div className="ranker_title"></div>
+                                    <div className="ranker_detail">총 {item.lcg_count_death} 데스</div>
                                 </Style.RankerContent>   
                             </Style.RankingItem>
                         )
@@ -74,13 +67,13 @@ const RankingViewDeath = (props : {data:{
                 </Style.RankingBox>
             </div>
             <div className="ranking_bottom">
-                {testArr.filter((lowRanking) => lowRanking.rank > 3).map((item, idx) => {
+                {props.data.filter((lowRanking) => lowRanking.rank > 3).map((item, idx) => {
                     return (
                         <div className="ranking_item" key={"lowRank_" + idx}>
                             <div className="item_rank">{item.rank}</div>
                             <div className="item_nickname">{item.lcg_nickname.split('#')[0]}</div>
-                            <div className="ranker_title">총 {item.lcg_count_death} 데스</div>
-                            <div className="item_detail"></div>
+                            <div className="item_title"></div>
+                            <div className="item_detail">총 {item.lcg_count_death} 데스</div>
                         </div>
                     )
                 })}
