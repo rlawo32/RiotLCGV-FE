0abcfa0ea9a1242d2a26bba0535171db39525f70
rlawo <rlawo32@naver.com>
2025-01-19 22:46:16 +0900

    [RiotLCGV][FE] <View> Ranking_킬_View 작업_20250119 (1)

--- src/app/ranking/match_ranking.tsx
+++ src/app/ranking/match_ranking.tsx
@@ -2,12 +2,13 @@
 
 import * as Style from "./match_ranking.style"
 
+import { useState } from "react";
 import { useQuery } from "@supabase-cache-helpers/postgrest-react-query";
-import { getLcgAllWinningRateQuery, getLcgAllDeathQuery } from "../queries/getLcgMatchRankingQuery";
+import { getLcgAllWinningRateQuery, getLcgAllKillQuery, getLcgAllDeathQuery } from "../queries/getLcgMatchRankingQuery";
 import useSupabaseBrowser from "../supabase-browser";
 
 import RankingViewWinningRate from "./rankingView_winningRate";
-import { useState } from "react";
+import RankingViewKill from "./rankingView_kill";
 import RankingViewDeath from "./rankingView_death";
 
 const MatchRanking = () => {
@@ -16,6 +17,7 @@ const MatchRanking = () => {
     const [rankSelect, setRankSelect] = useState<string>("AW");
     
     const { data: queryWinningRateResult } = useQuery(getLcgAllWinningRateQuery(supabase), {enabled: rankSelect === 'AW'});
+    const { data: queryAllKillResult } = useQuery(getLcgAllKillQuery(supabase), {enabled: rankSelect === 'AK'});
     const { data: queryAllDeathResult } = useQuery(getLcgAllDeathQuery(supabase), {enabled: rankSelect === 'AD'});
 
     return (
@@ -24,11 +26,13 @@ const MatchRanking = () => {
                 <div className="ranking_select">
                     <Style.RankingSelect onChange={(e) => setRankSelect(e.target.value)}>
                         <option value={"AW"}>총 승률</option>
+                        <option value={"AK"}>총 킬</option>
                         <option value={"AD"}>총 데스</option>
                     </Style.RankingSelect>
                 </div>
                 {
                     queryWinningRateResult && rankSelect === 'AW'   ? <RankingViewWinningRate data={queryWinningRateResult}/>   : 
+                    queryAllKillResult && rankSelect === 'AK'       ? <RankingViewKill data={queryAllKillResult}/>              : 
                     queryAllDeathResult && rankSelect === 'AD'      ? <RankingViewDeath data={queryAllDeathResult}/>            : <></>
                 }
             </div> 
