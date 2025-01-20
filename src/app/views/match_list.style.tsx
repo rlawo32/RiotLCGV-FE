3cf2a57374433d43f00cb9ba1671655675cced37
rlawo <rlawo32@naver.com>
2025-01-21 00:22:11 +0900

    [RiotLCGV][FE] <View> MoveGithub_View 작업_20250121 (3)

---
+++ src/app/views/match_list.style.tsx
@@ -0,0 +1,166 @@
+import styled from "styled-components";
+
+export const MatchList = styled('div')`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    height: 100%;
+    width: 1140px;
+    margin: auto;
+`;
+
+export const ListContainer = styled('div')`
+    position: relative;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    margin: 20px 0;
+
+    .matchHistory_box {
+        position: relative;
+        height: 0;
+        overflow: hidden;
+        transition: all .4s ease;
+
+        .box_inner {                                                                                                                  
+            position: relative;
+            display: flex;
+            align-items: center;
+            height: 100%;
+            padding: 20px 3px 24px;
+            z-index: 1;
+            border-radius: 14px;
+            background-color: #0e0e21;
+            background-image: url('https://cdn.prod.website-files.com/653bd35aae06cbc1b37167a4/653bd35aae06cbc1b3716999_blog-card-featured-image-ai-x-webflow-template.png');
+            background-position: 50%;
+            background-size: cover;
+        }
+
+        .box_outer {
+            position: absolute;
+            top: 0%;
+            bottom: 0%;
+            left: 0%;
+            right: 0%;
+            border-radius: 14px;
+            opacity: .4;
+            background-color: rgba(255, 255, 255, .05);
+            background-image: linear-gradient(to right, rgba(0, 0, 0, 0), rgba(255, 255, 255, .6) 50%, rgba(0, 0, 0, 0));
+        }
+    }
+      
+    .matchHistory_box.view_active {
+        position: relative;
+        display: inline-block;
+        padding: 2px;
+        text-decoration: none;
+        height: 435px;
+    }
+`
+
+export const ListBox = styled('div')`
+    position: relative;
+    display: inline-block;
+    height: 125px;
+    width: 850px;
+    margin: 10px 0;
+    padding: 1px;
+    border: none;
+    border-radius: 10px;
+    color: black;
+    font-weight: bold;
+    background-color: grey;
+    // background: linear-gradient(to right, 
+    /* rgba(50,10,245,0.8) 5%,  */
+    /* rgba(240,240,240,0.8) 50%,  */
+    /* rgba(245,10,50,0.8) 95%); */
+    cursor: pointer;
+
+    &::before, &::after {
+        border-radius: 10px;
+        content: '';
+        position: absolute;
+        left: -1px;
+        top: -1.5px;
+        background-image: linear-gradient(#bed8ff, #3d96ff);
+        background-size: 400%;
+        width: calc(100% + 2px);
+        height: calc(100% + 2px);
+        z-index: 0;
+        animation: steam 60s linear infinite;
+    }
+    
+    .box_inner {
+        position: relative;
+        height: 100%;
+        z-index: 1;
+        border-radius: 10px;
+        text-align: center;
+        text-decoration: none;
+        font-size: 18px;
+        font-weight: 500;
+        line-height: 1.143em;
+        background-color: #070719;
+        color: white;
+        box-shadow: 0 2px 8px rgba(61, 150, 255, .4), inset 0 -2px 12px rgba(58, 113, 255, .5);
+    }
+
+    .box_head {
+        display: flex;
+        align-items: center;
+        padding: 5px 0 0 7px;
+        font-size: 14px;
+    }
+
+    .box_body {
+        position: relative;
+        top: -3px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        padding-top: 6px;
+
+        .box_player {
+            position: relative;
+            display: flex;
+            align-items: center;
+            font-size: 18px;
+
+            .box_win {
+                position: absolute;
+                top: -54px;
+            }
+
+            .left_icon {
+                right: 85px;
+            }
+
+            .right_icon {
+                left: 85px;
+            }
+        }
+
+        .body_center {
+            position: relative;
+            top: -12px;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            margin: 0 30px;
+
+            .box_time {
+                position: absolute;
+                top: 55px;
+                font-size: 14px;
+            }
+        }
+    }
+
+    .box_foot {
+        display: flex;
+        align-items: center;
+        justify-content: right;
+        font-size: 10px;
+        padding: 0 7px 0;
+    }
+`;
---
+++ src/app/views/match_list.tsx
@@ -0,0 +1,108 @@
+'use client';
+
+import * as Style from "../component/match_list.style";
+import Image from "next/image";
+
+import { useRef, useState } from "react";
+import { useQuery } from "@supabase-cache-helpers/postgrest-react-query";
+import { getLcgMatchLogQuery } from "../queries/getLcgMatchLogQuery";
+import useSupabaseBrowser from "../supabase-browser";
+
+import MatchRanking from "../ranking/match_ranking";
+import MatchHistory from "./match_history";
+import LoadingSpinner from "../component/loading_spinner";
+
+const MatchList = () => {
+    const supabase = useSupabaseBrowser();
+    const matchHistoryRef:any = useRef<any>([]);
+
+    const [selectGameId, setSelectGameId] = useState<number>(0);
+    const [selectIdx, setSelectIdx] = useState<number>(-1);
+
+    const { data: lcgMatchLog, isLoading, isError } = useQuery(getLcgMatchLogQuery(supabase));
+
+    if (isLoading) {
+        return <LoadingSpinner />
+    }
+    
+    if (isError || !lcgMatchLog) {
+        return <div>Error</div>
+    }
+
+    const matchListBoxClick = (gameId:number, idx:number) => {
+        setSelectGameId(gameId);
+        setSelectIdx(idx);
+
+        if(!matchHistoryRef.current[idx].className.includes('view_active')) {
+            matchHistoryRef.current[idx].className += ' view_active';
+        } else {            
+            matchHistoryRef.current[idx].className = matchHistoryRef.current[idx].className.replace(' view_active', '');
+        }
+
+        for(let i:number=0; i<matchHistoryRef.current.length; i++) {
+            if(i !== idx) {
+                matchHistoryRef.current[i].className = matchHistoryRef.current[i].className.replace(' view_active', '');
+            }
+        }
+    }
+
+    return (
+        <div>
+            <MatchRanking />
+            <Style.MatchList>
+                {lcgMatchLog?.map((lcgLog, idx) => {
+                    return (
+                        <Style.ListContainer key={lcgLog.lcg_game_id}>
+                            <Style.ListBox onClick={() => matchListBoxClick(lcgLog.lcg_game_id, idx)}>
+                                <div className="box_inner">
+                                    <div className="box_head">
+                                        {(lcgLog.lcg_game_date).substring(0, 10)}
+                                    </div>
+                                    <div className="box_body">
+                                        <div className="box_player body_left">
+                                            {lcgLog.lcg_game_win === 100 ? <Image src={"/img/win_image.png"} alt={"WIN"} height={70} width={70} className="box_win left_icon" /> : <div className="item_win" />} 
+                                            <div>
+                                                {lcgLog.team_a_name_1}&nbsp;/&nbsp;
+                                                {lcgLog.team_a_name_2}&nbsp;/&nbsp; 
+                                                {lcgLog.team_a_name_3}&nbsp;/&nbsp; 
+                                                {lcgLog.team_a_name_4}&nbsp;/&nbsp; 
+                                                {lcgLog.team_a_name_5}
+                                            </div>                        
+                                        </div>
+                                        <div className="body_center">
+                                            <Image src={"/img/vs_image.png"} alt={"VS"} height={70} width={70} className="list_image" />
+                                            <div className="box_time">
+                                                {Math.floor(lcgLog.lcg_game_duration / 60)}:{String(lcgLog.lcg_game_duration % 60).padStart(2, '0')}
+                                            </div>
+                                        </div>
+                                        <div className="box_player body_right">
+                                            <div>
+                                                {lcgLog.team_b_name_1}&nbsp;/&nbsp;
+                                                {lcgLog.team_b_name_2}&nbsp;/&nbsp;
+                                                {lcgLog.team_b_name_3}&nbsp;/&nbsp;
+                                                {lcgLog.team_b_name_4}&nbsp;/&nbsp;
+                                                {lcgLog.team_b_name_5}
+                                            </div>
+                                            {lcgLog.lcg_game_win === 200 ? <Image src={"/img/win_image.png"} alt={"WIN"} height={70} width={70} className="box_win right_icon" /> : <div className="item_win" />} 
+                                        </div>
+                                    </div>
+                                    <div className="box_foot">
+                                        Ver. {lcgLog.lcg_game_ver}
+                                    </div>
+                                </div>
+                            </Style.ListBox>
+                            <div className="matchHistory_box" ref={(mh:any) => (matchHistoryRef.current[idx] = mh)}>
+                                <div className="box_inner">
+                                    {selectIdx === idx && <MatchHistory gameId={selectGameId} />}    
+                                </div>
+                                <div className="box_outer" />
+                            </div>
+                    </Style.ListContainer>
+                    )
+                })}
+            </Style.MatchList>
+        </div>
+    )
+}
+
+export default MatchList;
