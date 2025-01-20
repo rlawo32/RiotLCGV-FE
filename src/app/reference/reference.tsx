0efea3558f719a4017a15ddb3d6be62eb69aa835
sungjae <92013056+rlawo32@users.noreply.github.com>
2025-01-20 15:40:06 +0900

    Update reference.tsx

--- src/app/reference/reference.tsx
+++ src/app/reference/reference.tsx
@@ -59,6 +59,9 @@ const Reference = () => {
             }
         }
     }
+    
+    const randomArr:string[] = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R"];
+    const randomTest:string = randomArr[Math.floor(Math.random() * randomArr.length)];
 
     return (
         <ReferenceStyle>
