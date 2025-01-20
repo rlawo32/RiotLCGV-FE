dedb4449c45a4db6c48401ed4363bd2786d5a755
sungjae <92013056+rlawo32@users.noreply.github.com>
2025-01-20 16:10:10 +0900

    Update error_page.tsx

--- src/app/component/error_page.tsx
+++ src/app/component/error_page.tsx
@@ -1,6 +1,27 @@
 'use client'
 
-import { useEffect, useRef } from "react";
+import { memo, useEffect, useRef } from "react";
+import styled from "styled-components";
+
+const ErrorPageStyle = styled('div')`
+    color: white;
+
+    span {
+        animation: blink 1s infinite;
+    }
+
+    @keyframes blink {
+        0% {
+            opacity: 1;
+        }
+        50% {
+            opacity: 0;
+        }
+        100% {
+            opacity: 1;
+        }
+    }
+`
 
 const ErrorPage = (props : {errorCode:string, errorMessage:string}) => {
     const testRef:any = useRef<any>([]);
@@ -11,31 +32,32 @@ const ErrorPage = (props : {errorCode:string, errorMessage:string}) => {
         testRef.current[n].innerHTML = "";
 
         setTimeout(function() {
-            const se = setInterval(function() {
+            setInterval(function() {
+                if (i === str.length+1) {
+                    return;
+                }
+                testRef.current[n].innerHTML = str.slice(0, i) + '<span>|</span>';
                 i++;
-                testRef.current[n].innerHTML = str.slice(0, i) + "|";
             }, 10);
-            if (i == str.length) {
-                clearInterval(se);
-                testRef.current[n].innerHTML = str;
-            }
         }, t);
     }
 
     useEffect(() => {
-        if(props.errorCode.length > 0) {
-            test(0, 0);
-            test(1, 600);
-        }
+        test(0, 10);
+        test(1, 20);
+        test(2, 30);
     }, [props])
 
     return (
-        <div>
+        <ErrorPageStyle>
             <p>HTTP: 404</p>
             <div ref={(te:any) => (testRef.current[0] = te)}>{props.errorCode}</div>
             <div ref={(te:any) => (testRef.current[1] = te)}>{props.errorMessage}</div>
-        </div>
+            <div ref={(te:any) => (testRef.current[2] = te)}>else if we_screwed_up alert "We're really sorry about that." window. location</div>
+        </ErrorPageStyle>
     )
 }
+
+const ErrorPageRes = memo(ErrorPage);
 
 export default ErrorPage;
