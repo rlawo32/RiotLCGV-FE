239e847ed03aa871226951bbe2a2ec1f04a725fc
rlawo <rlawo32@naver.com>
2025-01-21 23:26:24 +0900

    [RiotLCGV][FE] <View> HighRanking_best3_cs_작업_20250121 (1)

---
+++ src/app/ranking/rankingView_1G3B_cs.tsx
@@ -0,0 +1,75 @@
+'use client';
+
+import * as Style from "./match_ranking.style"
+import Image from "next/image";
+import Link from "next/link";
+
+import { duplicationRank } from "./ranking_tool";
+
+const RankingView1G3BCs = (props : {data:{
+            lcg_summoner_puuid: string
+            lcg_summoner_nickname: string
+            cs: number
+            rank: number
+        }[]}) => {
+
+    return (
+        <>
+            <div className="ranking_top">    
+                <Style.RankingBox $ea={props.data.filter((item) => item.rank === 2).length}>
+                    {props.data.filter((highRanking) => highRanking.rank === 2).map((item, idx, arr) => {
+                        return (
+                            <Style.HighRankingItem $ea={arr.length} $rank={2} key={"rank2_" + idx}>
+                                <Image src={"/img/border_silver_image.png"} alt={"ranking_border"} 
+                                height={duplicationRank(arr.length)} width={duplicationRank(arr.length)} className="ranker_img" />
+                                <Style.RankerContent $ea={arr.length}>
+                                    <Link href={"https://www.op.gg/summoners/kr/" + item.lcg_summoner_nickname.split('#')[0] + "-" + item.lcg_summoner_nickname.split('#')[1]} target="_blank">
+                                        <div className="ranker_name">{item.lcg_summoner_nickname.split('#')[0]}</div>
+                                    </Link>
+                                    <div className="ranker_title">최고 {item.cs.toLocaleString()} CS</div>
+                                    <div className="ranker_detail"></div>
+                                </Style.RankerContent>
+                            </Style.HighRankingItem>
+                        )
+                    })}
+                </Style.RankingBox>
+                <Style.RankingBox $ea={props.data.filter((item) => item.rank === 1).length}>
+                    {props.data.filter((highRanking) => highRanking.rank === 1).map((item, idx, arr) => {
+                        return (
+                            <Style.HighRankingItem $ea={arr.length} $rank={1} key={"rank1_" + idx}>
+                                <Image src={"/img/border_gold_image.png"} alt={"ranking_border"} 
+                                height={duplicationRank(arr.length)} width={duplicationRank(arr.length)} className="ranker_img" />
+                                <Style.RankerContent $ea={arr.length}>
+                                    <Link href={"https://www.op.gg/summoners/kr/" + item.lcg_summoner_nickname.split('#')[0] + "-" + item.lcg_summoner_nickname.split('#')[1]} target="_blank">
+                                        <div className="ranker_name">{item.lcg_summoner_nickname.split('#')[0]}</div>
+                                    </Link>
+                                    <div className="ranker_title">최고 {item.cs.toLocaleString()} CS</div>
+                                    <div className="ranker_detail"></div>
+                                </Style.RankerContent>
+                            </Style.HighRankingItem>
+                        )
+                    })}
+                </Style.RankingBox>
+                <Style.RankingBox $ea={props.data.filter((item) => item.rank === 3).length}>
+                    {props.data.filter((highRanking) => highRanking.rank === 3).map((item, idx, arr) => {
+                        return (
+                            <Style.HighRankingItem $ea={arr.length} $rank={3} key={"rank3_" + idx}>
+                                <Image src={"/img/border_bronze_image.png"} alt={"ranking_border"} 
+                                height={duplicationRank(arr.length)} width={duplicationRank(arr.length)} className="ranker_img" />
+                                <Style.RankerContent $ea={arr.length}>
+                                    <Link href={"https://www.op.gg/summoners/kr/" + item.lcg_summoner_nickname.split('#')[0] + "-" + item.lcg_summoner_nickname.split('#')[1]} target="_blank">
+                                        <div className="ranker_name">{item.lcg_summoner_nickname.split('#')[0]}</div>
+                                    </Link>
+                                    <div className="ranker_title">최고 {item.cs.toLocaleString()} CS</div>
+                                    <div className="ranker_detail"></div>
+                                </Style.RankerContent>   
+                            </Style.HighRankingItem>
+                        )
+                    })}
+                </Style.RankingBox>
+            </div>
+        </>
+    )
+}
+
+export default RankingView1G3BCs;
