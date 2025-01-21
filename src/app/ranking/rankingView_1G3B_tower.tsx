e0ac2c321b9733db208a8341c3c7dccdd623e926
rlawo <rlawo32@naver.com>
2025-01-21 23:26:52 +0900

    [RiotLCGV][FE] <View> HighRanking_best3_tower_작업_20250121 (1)

---
+++ src/app/ranking/rankingView_1G3B_tower.tsx
@@ -0,0 +1,77 @@
+'use client';
+
+import * as Style from "./match_ranking.style"
+import Image from "next/image";
+import Link from "next/link";
+
+import { duplicationRank } from "./ranking_tool";
+import TurretIcon from "../icons/TurretIcon";
+
+const RankingView1G3BTower = (props : {data:{
+            lcg_summoner_puuid: string
+            lcg_summoner_nickname: string
+            lcg_destroy_tower: number
+            lcg_damage_tower: number
+            rank: number
+        }[]}) => {
+
+    return (
+        <>
+            <div className="ranking_top">    
+                <Style.RankingBox $ea={props.data.filter((item) => item.rank === 2).length}>
+                    {props.data.filter((highRanking) => highRanking.rank === 2).map((item, idx, arr) => {
+                        return (
+                            <Style.HighRankingItem $ea={arr.length} $rank={2} key={"rank2_" + idx}>
+                                <Image src={"/img/border_silver_image.png"} alt={"ranking_border"} 
+                                height={duplicationRank(arr.length)} width={duplicationRank(arr.length)} className="ranker_img" />
+                                <Style.RankerContent $ea={arr.length}>
+                                    <Link href={"https://www.op.gg/summoners/kr/" + item.lcg_summoner_nickname.split('#')[0] + "-" + item.lcg_summoner_nickname.split('#')[1]} target="_blank">
+                                        <div className="ranker_name">{item.lcg_summoner_nickname.split('#')[0]}</div>
+                                    </Link>
+                                    <div className="ranker_title">최고 <TurretIcon />{item.lcg_destroy_tower} 파괴</div>
+                                    <div className="ranker_detail">최고 {item.lcg_damage_tower.toLocaleString()} 피해량</div>
+                                </Style.RankerContent>
+                            </Style.HighRankingItem>
+                        )
+                    })}
+                </Style.RankingBox>
+                <Style.RankingBox $ea={props.data.filter((item) => item.rank === 1).length}>
+                    {props.data.filter((highRanking) => highRanking.rank === 1).map((item, idx, arr) => {
+                        return (
+                            <Style.HighRankingItem $ea={arr.length} $rank={1} key={"rank1_" + idx}>
+                                <Image src={"/img/border_gold_image.png"} alt={"ranking_border"} 
+                                height={duplicationRank(arr.length)} width={duplicationRank(arr.length)} className="ranker_img" />
+                                <Style.RankerContent $ea={arr.length}>
+                                    <Link href={"https://www.op.gg/summoners/kr/" + item.lcg_summoner_nickname.split('#')[0] + "-" + item.lcg_summoner_nickname.split('#')[1]} target="_blank">
+                                        <div className="ranker_name">{item.lcg_summoner_nickname.split('#')[0]}</div>
+                                    </Link>
+                                    <div className="ranker_title">최고 <TurretIcon />{item.lcg_destroy_tower} 파괴</div>
+                                    <div className="ranker_detail">최고 {item.lcg_damage_tower.toLocaleString()} 피해량</div>
+                                </Style.RankerContent>
+                            </Style.HighRankingItem>
+                        )
+                    })}
+                </Style.RankingBox>
+                <Style.RankingBox $ea={props.data.filter((item) => item.rank === 3).length}>
+                    {props.data.filter((highRanking) => highRanking.rank === 3).map((item, idx, arr) => {
+                        return (
+                            <Style.HighRankingItem $ea={arr.length} $rank={3} key={"rank3_" + idx}>
+                                <Image src={"/img/border_bronze_image.png"} alt={"ranking_border"} 
+                                height={duplicationRank(arr.length)} width={duplicationRank(arr.length)} className="ranker_img" />
+                                <Style.RankerContent $ea={arr.length}>
+                                    <Link href={"https://www.op.gg/summoners/kr/" + item.lcg_summoner_nickname.split('#')[0] + "-" + item.lcg_summoner_nickname.split('#')[1]} target="_blank">
+                                        <div className="ranker_name">{item.lcg_summoner_nickname.split('#')[0]}</div>
+                                    </Link>
+                                    <div className="ranker_title">최고 <TurretIcon />{item.lcg_destroy_tower} 파괴</div>
+                                    <div className="ranker_detail">최고 {item.lcg_damage_tower.toLocaleString()} 피해량</div>
+                                </Style.RankerContent>   
+                            </Style.HighRankingItem>
+                        )
+                    })}
+                </Style.RankingBox>
+            </div>
+        </>
+    )
+}
+
+export default RankingView1G3BTower;
