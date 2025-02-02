7e2e9368766116e4f0e18cc104c94d7feba410f3
rlawo <rlawo32@naver.com>
2025-02-02 23:39:30 +0900

    [RiotLCGV][FE] <Icon> MvpIcon_추가_20250202

---
+++ src/app/icons/MvpIcon.tsx
@@ -0,0 +1,29 @@
+'use client'
+
+import styled from "styled-components";
+
+const IconStyle = styled('div')<{$rank:string}>`
+    height: 14px;
+    width: fit-content;
+    padding: 0 8px;
+    margin: 2px auto 0;
+    border: none;
+    border-radius: 9px;
+    background-color: ${({$rank}) => $rank.includes("M") ? "#EB9C00" : $rank.includes("A") ? "#7D59EA" : "#9E9EB1"};
+    color: rgb(255, 255, 255);
+    text-align: center;
+    line-height: 14px;
+    font-size: 11px;
+    font-weight: normal;
+`
+
+const MvpIcon = (props : {rank:string}) => {
+
+    return (
+        <IconStyle $rank={props.rank}>
+            {props.rank.includes("M") ? "MVP" : props.rank.includes("A") ? "ACE" : props.rank.substring(1) + "th"}
+        </IconStyle>
+    )
+}
+
+export default MvpIcon;
