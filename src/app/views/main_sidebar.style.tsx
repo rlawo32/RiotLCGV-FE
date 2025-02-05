cec0783d65d397d6cc27a8f1a6f10ce09852f740
rlawo <rlawo32@naver.com>
2025-02-06 01:47:07 +0900

    [RiotLCGV][FE] <View> MainSidebar_작업_20250206 (1)

---
+++ src/app/views/main_sidebar.style.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const MainSidebar = styled('div')`
+    position: relative;
+    height: 100%;
+    width: 300px;
+    border-radius: 10px;
+    background-color: rgb(49 49 60 / .7);
+`
---
+++ src/app/views/main_sidebar.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import * as Style from "./main_sidebar.style";
+
+const MainSidebar = () => {
+
+    return (
+        <Style.MainSidebar>
+
+        </Style.MainSidebar>
+    )
+}
+
+export default MainSidebar;
