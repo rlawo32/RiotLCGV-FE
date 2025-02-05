046e70411ebea78d304669b3ee7f16a83027c9b6
rlawo <rlawo32@naver.com>
2025-02-06 01:46:23 +0900

    [RiotLCGV][FE] <CSS> FontSize_rem_작업_20250206

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import MainView from "./views/main_view";
+import MoveGithub from "./component/move_github";
 
 export default function Home() {
   return (
     <div>
+      <MoveGithub />
       <MainView />
     </div>
   );
