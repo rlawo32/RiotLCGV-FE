f8eeddeb8b50cbb62b21a9f24d1a181649cfc138
rlawo <rlawo32@naver.com>
2025-02-06 01:43:53 +0900

    [RiotLCGV][FE] <Icon> MvpIcon_수정_20250206

--- src/app/icons/MvpIcon.tsx
+++ src/app/icons/MvpIcon.tsx
@@ -3,7 +3,7 @@
 import styled from "styled-components";
 
 const IconStyle = styled('div')<{$rank:string}>`
-    height: 14px;
+    height: 15px;
     width: fit-content;
     padding: 0 8px;
     margin: 2px auto 0;
@@ -12,8 +12,8 @@ const IconStyle = styled('div')<{$rank:string}>`
     background-color: ${({$rank}) => $rank.includes("M") ? "#EB9C00" : $rank.includes("A") ? "#7D59EA" : "#9E9EB1"};
     color: rgb(255, 255, 255);
     text-align: center;
-    line-height: 14px;
-    font-size: 11px;
+    line-height: 15px;
+    font-size: 1.1rem;
     font-weight: normal;
 `
 
