b00dfd374ef505b180f668f984c5672fddeecbf3
rlawo <rlawo32@naver.com>
2025-02-06 01:45:12 +0900

    [RiotLCGV][FE] <SUPABASE> LcgMatchLog_Query_추가_20250206

--- src/app/queries/getLcgMatchLogQuery.ts
+++ src/app/queries/getLcgMatchLogQuery.ts
@@ -6,3 +6,11 @@ export function getLcgMatchLogQuery(client: TypedSupabaseClient) {
     .select("*")
     .order("lcg_game_id", { ascending: false })
 }
+
+export function getLcgMatchLogLatestQuery(client: TypedSupabaseClient) {
+  return client
+    .from("lcg_match_log")
+    .select("*")
+    .order("lcg_game_id", { ascending: false })
+    .limit(1)
+}
