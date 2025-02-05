2826fda70b0dda9f9722528aad2d5cd656ea936f
rlawo <rlawo32@naver.com>
2025-02-06 01:43:36 +0900

    [RiotLCGV][FE] <Icon> TeamColorIcon_추가_20250206

---
+++ src/app/icons/TeamBlueIcon.tsx
@@ -0,0 +1,15 @@
+
+const TeamBlueIcon = () => {
+
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="grey" viewBox="0 0 24 24">
+            <g fill="none" fillRule="evenodd">
+                <path fill="#1F85FD" d="m3 5 4 4v8h8l4 4H3z"></path>
+                <path fill="#A3A4A9" d="m21 19-4-4V7H9L5 3h16z" opacity="0.4"></path>
+                <path fill="#A3A4A9" d="M10 14v-4h4v4z" opacity="0.4"></path>
+            </g>
+        </svg>
+    )
+}
+
+export default TeamBlueIcon;
---
+++ src/app/icons/TeamBlueRed.tsx
@@ -0,0 +1,15 @@
+
+const TeamRedIcon = () => {
+
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="grey" viewBox="0 0 24 24">
+            <g fill="none" fillRule="evenodd">
+                <path fill="#F60C50" d="m21 19-4-4V7H9L5 3h16z"></path>
+                <path fill="#A3A4A9" d="m3 5 4 4v8h8l4 4H3z" opacity="0.4"></path>
+                <path fill="#A3A4A9" d="M14 10v4h-4v-4z" opacity="0.4"></path>
+            </g>
+        </svg>
+    )
+}
+
+export default TeamRedIcon;
