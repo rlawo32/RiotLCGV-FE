8cabe3a040a0abd51b084c6348d7492d317a1c4e
rlawo <rlawo32@naver.com>
2025-02-07 01:19:27 +0900

    [RiotLCGV][FE] <View> MainSidebar_작업_20250207 (2)

--- src/app/views/main_sidebar.style.tsx
+++ src/app/views/main_sidebar.style.tsx
@@ -2,8 +2,47 @@ import styled from "styled-components";
 
 export const MainSidebar = styled('div')`
     position: relative;
-    height: 100%;
+    display: flex;
+    flex-direction: column;
+    height: fit-content;
     width: 300px;
+    padding: 40px 10px;
     border-radius: 10px;
     background-color: rgb(49 49 60 / .7);
+
+    .btn_icon {
+        position: absolute;
+        top: 10px;
+        left: 25px;
+        font-size: 2.5rem;
+        color: white;
+    }
+
+    button {
+        position: relative;
+        padding: 15px 15px 10px 75px;
+        margin: 4px 0;
+        border: none;
+        border-radius: 10px;
+        background: none;
+        text-align: left;
+        font-size: 1.7rem;
+        font-weight: 400;
+        color: white;
+        cursor: pointer;
+        z-index: 3;
+
+        &:hover {
+            background-color: rgb(89 89 100 / 1);
+            transition: .2s ease-in-out;
+        }
+
+        &:not(:hover) {
+            transition: .2s ease-in-out;
+        }
+    }
+
+    .select_active {
+        background-color: rgb(89 89 100 / 1);
+    }
 `
--- src/app/views/main_sidebar.tsx
+++ src/app/views/main_sidebar.tsx
@@ -2,11 +2,55 @@
 
 import * as Style from "./main_sidebar.style";
 
-const MainSidebar = () => {
+import React, { useEffect, useRef, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faUsers as icon_player, faGamepad as icon_latest, 
+    faScroll as icon_history, faTrophy as icon_ranking
+} from "@fortawesome/free-solid-svg-icons";
+
+const MainSidebar = (props : {changeView:any}) => {
+    const btnRef:any = useRef<any>([]);
+    const [selectView, setSelectView] = useState<number>(0);
+
+    const btnClick = (idx:number) => {
+        props.changeView(idx);
+
+        if(!btnRef.current[idx].className.includes('select_active')) {
+            btnRef.current[idx].className += ' select_active';
+        } else {            
+            btnRef.current[idx].className = btnRef.current[idx].className.replace(' select_active', '');
+        }
+
+        for(let i:number=0; i<btnRef.current.length; i++) {
+            if(i !== idx) {
+                btnRef.current[i].className = btnRef.current[i].className.replace(' select_active', '');
+            }
+        }
+    }
+
+    useEffect(() => {
+        btnRef.current[selectView].className += ' select_active';
+    }, [])
 
     return (
         <Style.MainSidebar>
-
+            <button onClick={() => btnClick(0)} ref={(btn:any) => (btnRef.current[0] = btn)}>
+                <FontAwesomeIcon icon={icon_latest} className="btn_icon"/>
+                최근 게임
+            </button>
+            <button onClick={() => btnClick(1)} ref={(btn:any) => (btnRef.current[1] = btn)}>
+                <FontAwesomeIcon icon={icon_history} className="btn_icon"/>
+                내전 전적
+            </button>
+            <button onClick={() => btnClick(2)} ref={(btn:any) => (btnRef.current[2] = btn)}>
+                <FontAwesomeIcon icon={icon_ranking} className="btn_icon"/>
+                스코어 랭킹
+            </button>
+            <button onClick={() => btnClick(3)} ref={(btn:any) => (btnRef.current[3] = btn)}>
+                <FontAwesomeIcon icon={icon_player} className="btn_icon"/>
+                플레이어
+            </button>
         </Style.MainSidebar>
     )
 }
