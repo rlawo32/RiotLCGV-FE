37ec6b5b7199722f3f85e2514b6d254cee739b2a
rlawo <rlawo32@naver.com>
2025-02-08 17:30:10 +0900

    [RiotLCGV][FE] <SUPABASE> DataType_설정_20250208 (13)

--- src/app/queries/getLcgMatchInfoQuery.ts
+++ src/app/queries/getLcgMatchInfoQuery.ts
@@ -3,6 +3,6 @@ import { TypedSupabaseClient } from "../supabase"
 export function getLcgMatchInfoQuery(client:TypedSupabaseClient, gameId:number) {
   return client
     .from("lcg_match_info")
-    .select("lcg_game_id, lcg_ver_main, lcg_ver_cdn, lcg_game_duration, lcg_ver_lang, lcg_max_damage_total, lcg_max_damage_taken")
+    .select("lcg_game_id, lcg_ver_main, lcg_game_duration, lcg_max_gold, lcg_max_crowd, lcg_max_dpm, lcg_max_gpm, lcg_max_dpg, lcg_max_damage_total, lcg_max_damage_taken")
     .eq("lcg_game_id", gameId)
 }
