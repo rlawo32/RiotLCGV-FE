1d0bf8c7c6b2a3051d751acb164519f105ab3861
rlawo <rlawo32@naver.com>
2025-02-08 20:48:13 +0900

    [RiotLCGV][FE] <View> UI_개선_작업(Graph View)_20250208 (1)

--- src/app/component/damage_graph.tsx
+++ src/app/component/damage_graph.tsx
@@ -2,32 +2,30 @@
 
 import styled from "styled-components";
 
-const GraphTotal = styled('div')`
-    height: 13px;
-    width: 50px;
+const GraphTotal = styled('div')<{$flag:string}>`
+    height: ${({$flag}) => $flag === 'D' || $flag === 'T' ? "11px" : "16px"};
+    width: ${({$flag}) => $flag === 'D' || $flag === 'T' ? "50px" : "100px"};
     margin: 5px auto;
-    padding: 1px;
-    background-color: #31313C;
-    border: 1px solid #3d96ff;
-    border-radius: 5px;
+    background-color: rgb(28 28 31 / 1);
+    border-radius: 4px;
+    overflow: hidden;
 `;
 
-const GraphTarget = styled('div')<{$data:number, $color:string}>`
-    height: 9px;
+const GraphTarget = styled('div')<{$data:number, $flag:string}>`
+    height: 100%;
     width: ${({$data}) => $data + "%"};
-    background-color: ${({$color}) => $color};
-    border: 1px solid ${({$color}) => $color};
-    border-radius: 5px;
+    background-color: ${({$flag}) => $flag === 'T' ? "#7B7A8E" : "#E84057"};
+    border-radius: 2px;
 `;
 
-const DamageGraph = (props : {standard:number, target:number, flag:string}) => {
+const DamageGraph = (props : {standard:number|undefined, target:number, flag:string}) => {
 
     // division - D : 피해량, T : 받은 피해량
     // 일부 값 / 기준 값 * 100
 
     return (
-        <GraphTotal>
-            <GraphTarget $data={props.target / props.standard * 100} $color={props.flag === 'D' ? '#E84057' : '#7B7A8E'} />
+        <GraphTotal $flag={props.flag}>
+            <GraphTarget $data={props.target / (!!props.standard ? props.standard : 0) * 100} $flag={props.flag} />
         </GraphTotal>
     )
 }
