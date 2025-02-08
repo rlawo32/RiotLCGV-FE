1101eb53e8b2c2485cd3d346f0d1798e437dd7f1
rlawo <rlawo32@naver.com>
2025-02-08 16:56:55 +0900

    [RiotLCGV][FE] <View> UI_개선_작업(MatchAnalyze View)_20250208 (1)

---
+++ src/app/views/match_analyze.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import * as Style from "./match_analyze.stlye";
+
+const MatchAnalyze = () => {
+
+    return (
+        <Style.MatchAnalyze>
+
+        </Style.MatchAnalyze>
+    )
+}
+
+export default MatchAnalyze;
