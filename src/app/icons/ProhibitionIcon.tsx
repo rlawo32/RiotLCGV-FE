9b77f218f63994054aa95637deaa7462cc8583b6
rlawo <rlawo32@naver.com>
2025-02-08 16:55:42 +0900

    [RiotLCGV][FE] <Icon> ProhibitionIcon_추가_20250208

---
+++ src/app/icons/ProhibitionIcon.tsx
@@ -0,0 +1,12 @@
+
+const ProhibitionIcon = () => {
+
+    return (
+        <svg width="100%" height="100%" viewBox="0 0 64 64" preserveAspectRatio="none">
+            <line x1="64" y1="0" x2="0" y2="64" vectorEffect="non-scaling-stroke" stroke="red">
+            </line>
+        </svg>
+    )
+}
+
+export default ProhibitionIcon;
