0b3f309acf15196b57580f6a865b3aee2bfa189f
rlawo <rlawo32@naver.com>
2025-02-21 01:13:54 +0900

    [RiotLCGV][FE] <View> MatchPlayer_작업_20250221 (1)

---
+++ src/app/views/match_player.style.tsx
@@ -0,0 +1,357 @@
+import styled from "styled-components";
+
+export const MatchPlayer = styled('div')`
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+    width: 768px;
+    height: 100%;
+    min-height: 860px;
+    margin: 0 auto;
+    padding: 7px 30px;
+    border-radius: 10px;    
+    background-color: rgb(49 49 60 / .7);
+`;
+
+export const PlayerSelectBox = styled('div')`
+    position: relative;
+    display: flex;
+    flex-wrap: wrap;
+    margin: 10px auto;
+    padding: 10px 20px;
+    border-bottom: 1px solid rgb(255 68 56 / 1);
+
+    .select_item {
+        margin: 3px;
+        padding: 7px 12px;
+        border-radius: 10px;
+        border: none;
+        background-color: rgb(30 30 38 / 1);
+        font-size: 1.3rem;
+        cursor: pointer;
+
+        &:hover {
+            background-color: rgb(255 68 56 / 1);
+        }
+    }
+`;
+
+export const PlayerDataBox = styled('div')`
+    position: relative;
+    display: flex;
+    width: 100%;
+    flex-direction: column;
+    margin: 10px auto;
+
+    .player_img {
+        height: 80px;
+        height: 80px;
+        border-radius: 35px;
+    }
+
+    .rank_img {
+        height: 80px;
+        height: 80px;
+        object-fit: cover;
+        transform: scale(1.4);
+    }
+
+    .champion_img {
+        height: 35px;
+        height: 35px;
+        border-radius: 15px;
+        object-fit: cover;
+    }
+
+    .box_head {
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        height: 170px;
+        width: 100%;
+        padding: 20px 30px;
+        border-radius: 10px;
+        background-color: rgb(30 30 38 / 1);
+        
+        .head_section  {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+        }
+
+        .head_player {
+
+            .player_name {
+                margin-top: 12px;
+                font-size: 1.2rem;
+            }
+        }
+
+        .head_rank {
+
+            .rank_desc {
+                margin-top: 10px;
+                font-size: 1.2rem;
+            }
+        }
+
+        .head_data {
+            display: flex;
+            align-items: center;
+
+            .head_graph {
+                position: relative;
+                height: 108px;
+                width: 108px;
+
+                .graph_gage {
+                    position: absolute;
+                    height: 108px;
+                    width: 108px;
+                    text-align: center;
+                    color: #3182ce;
+                    font-size: 1.6rem;
+                    line-height: 108px;
+                }
+            }
+
+            .head_detail {
+                display: flex;
+                flex-direction: column;
+                margin-left: 25px;
+
+                .detail_play {
+                    margin-bottom: 20px;
+
+                    .count_play {
+                        font-size: 1.5rem;
+                        font-weight: 700;
+                    }
+
+                    .count_wof {
+                        padding-top: 2px;
+                        font-size: 1.2rem;
+                        font-weight: 400;
+                        color: rgb(123 122 142 / 1);
+                    }
+                }
+
+                .detail_kda {
+
+                    .calc_kda {
+                        font-size: 1.5rem;
+                        font-weight: 700;
+                    }
+
+                    .origin_kda {
+                        padding-top: 2px;
+                        font-size: 1.2rem;
+                        font-weight: 700;
+                        color: rgb(123 122 142 / 1);
+
+                        span:nth-child(1) {
+                            margin: 0 4px 0 0;
+                        }
+
+                        span:nth-child(1n+2) {
+                            margin: 0 4px;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    .box_body {
+        display: flex;
+        justify-content: space-between;
+        height: 100%;
+        width: 100%;
+        margin-top: 20px;
+        border-radius: 10px;
+        
+        .body_section  {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            width: 100%;
+            margin: 5px;
+            border-radius: 10px;
+            background-color: rgb(30 30 38 / 1);
+            padding: 15px 25px;
+            text-align: center;
+
+            .relative_head {
+                display: flex;
+                justify-content: space-between;
+                align-items: center;
+                width: 100%;
+                padding: 5px 10px;
+                border-bottom: 1px solid rgb(80 80 98 / 1);
+                font-size: 1.2rem;
+                font-weight: 700;
+
+                .head_opponent {
+                    width: 100px;
+                    text-align: left;
+                }
+
+                .head_winningRate {
+                    width: 120px;
+                }
+
+                .head_matchLine {
+                    width: 40px;
+                }
+            }
+
+            .relative_item {
+                display: flex;
+                justify-content: space-between;
+                align-items: center;
+                height: 45px;
+                width: 100%;
+                margin-top: 3px;
+                padding: 7px 10px;
+                border-bottom: 1px solid rgb(80 80 98 / 1);
+
+                &:hover {
+                    background-color: rgb(40 40 48 / 1);
+                }
+
+                .player_opponent {
+                    width: 100px;
+                    font-size: 1.3rem;
+                    text-align: left;
+                    white-space: nowrap;
+                    overflow: hidden;
+                    text-overflow: ellipsis;
+                }
+
+                .player_winningRate {
+                    display: flex;
+                    flex-direction: column;
+                    align-items: center;
+                    width: 120px;
+                    font-weight: 700;
+
+                    .match_detail {
+                        font-size: 1.2rem;
+                    }
+
+                    .match_winning {
+                        padding-top: 3px;
+                        font-size: 1rem;
+                        color: rgb(123 122 142 / 1);
+                    }
+                }
+
+                .player_matchLine {
+                    width: 40px;
+                }
+            }
+
+            .champion_head {
+                display: flex;
+                justify-content: space-between;
+                align-items: center;
+                width: 100%;
+                padding: 5px 10px;
+                border-bottom: 1px solid rgb(80 80 98 / 1);
+                font-size: 1.2rem;
+                font-weight: 700;
+
+                .head_champion {
+                    width: 50px;
+                    text-align: left;
+                }
+
+                .head_kda {
+                    width: 120px;
+                }
+
+                .head_winningRate {
+                    width: 70px;
+                }
+            }
+
+            .champion_item {
+                display: flex;
+                justify-content: space-between;
+                align-items: center;
+                height: 45px;
+                width: 100%;
+                margin-top: 3px;
+                padding: 5px 10px;
+                border-bottom: 1px solid rgb(80 80 98 / 1);
+
+                &:hover {
+                    background-color: rgb(40 40 48 / 1);
+                }
+
+                .player_champion {
+                    width: 50px;
+                    text-align: left;
+                }
+                
+                .player_kda {
+                    display: flex;
+                    flex-direction: column;
+                    align-items: center;
+                    width: 120px;
+                    font-weight: 700;
+
+                    .match_detail {
+                        font-size: 1.2rem;
+
+                        span:nth-child(1) {
+                            margin: 0 4px 0 0;
+                        }
+
+                        span:nth-child(1n+2) {
+                            margin: 0 4px;
+                        }
+                    }
+
+                    .match_kda {
+                        padding-top: 3px;
+                        font-size: 1rem;
+                        color: rgb(123 122 152 / 1);
+                    }
+                }
+
+                .player_winningRate {
+                    width: 70px;
+                    font-size: 1.1rem;
+
+                    span {
+                        margin: 0 1px;
+                        letter-spacing: .15rem;
+                    }
+                }
+            }
+        }
+    }
+`;
+
+export const LcgKdaCalc = styled('div')<{$k:number, $d:number, $a:number}>`
+    display: flex;
+    align-items: center;
+
+    .kda {
+        margin: 0 0 0 4px;
+        color: ${({$k, $d, $a}) => 
+            ($k > 0 || $a > 0) && $d == 0 ? "#FF8200"
+            :
+            Math.floor(($k + $a) / $d) > 4 ? "#FF8200" 
+            :
+            Math.floor(($k + $a) / $d) > 3 ? "#0093FF"
+            :
+            Math.floor(($k + $a) / $d) > 2 ? "#00BBA3"
+            :
+            "#9E9EB1"
+        }
+    }
+`;
---
+++ src/app/views/match_player.tsx
@@ -0,0 +1,231 @@
+'use client'
+
+import * as Style from "./match_player.style";
+
+import { useEffect, useState } from "react";
+import { useQuery } from "@supabase-cache-helpers/postgrest-react-query";
+import useSupabaseBrowser from "../supabase-browser";
+import { getLcgPlayerDataQuery, getSelectLcgPlayerChampionQuery, 
+    getSelectLcgAllKdaQuery, getSelectLcgWinningRateQuery,
+    getSelectLcgPlayerDataQuery, getSelectLcgPlayerRelativeQuery } from "../queries/getLcgPlayerDataQuery";
+import { getLcgMatchEtcQuery } from "../queries/getLcgMatchEtcQuery";
+
+import TopIcon from "../icons/topIcon";
+import JugIcon from "../icons/jugIcon";
+import MidIcon from "../icons/midIcon";
+import AdcIcon from "../icons/adcIcon";
+import SupIcon from "../icons/supIcon";
+
+const MatchPlayer = () => {
+    const supabase = useSupabaseBrowser();
+
+    let imageUrl1:string = "";
+    let imageUrl2:string = "";
+
+    const { data: lcgMatchEtc } = useQuery(getLcgMatchEtcQuery(supabase), {});
+    const { data: lcgPlayerData } = useQuery(getLcgPlayerDataQuery(supabase), {});
+
+    const [selectPlayer, setSelectPlayer] = useState<string>("");
+    const { data: selectPlayerData } = useQuery(getSelectLcgPlayerDataQuery(supabase, selectPlayer), {});
+    const { data: selectPlayerAllKda } = useQuery(getSelectLcgAllKdaQuery(supabase, selectPlayer), {});
+    const { data: selectPlayerWinningRate } = useQuery(getSelectLcgWinningRateQuery(supabase, selectPlayer), {});
+    const { data: selectPlayerChampion } = useQuery(getSelectLcgPlayerChampionQuery(supabase, selectPlayer), {});
+    const { data: selectPlayerRelative } = useQuery(getSelectLcgPlayerRelativeQuery(supabase, selectPlayer), {});
+
+    if(!!lcgMatchEtc) {
+        imageUrl1 = lcgMatchEtc[0].lcg_main_image;
+        imageUrl2 = lcgMatchEtc[0].lcg_sub_image;
+    }
+
+    useEffect(() => {
+        if(!!lcgPlayerData) {
+            setSelectPlayer(lcgPlayerData[0].lcg_summoner_puuid);
+        }
+    }, [])
+
+    return (
+        <Style.MatchPlayer>
+            <Style.PlayerSelectBox>
+                {lcgPlayerData?.map((data, idx) => {
+                    return (
+                        <div className="select_item" onClick={() => setSelectPlayer(data.lcg_summoner_puuid)} key={"select_" + idx}>
+                            {data.lcg_summoner_nickname}
+                        </div>
+                    )
+                })}
+            </Style.PlayerSelectBox>
+            <Style.PlayerDataBox>
+                <div className="box_head">
+                    {
+                        !!selectPlayerData ? 
+                            <>
+                                <div className="head_section head_player">
+                                    <img src={imageUrl1 + "profileicon/" + selectPlayerData[0].lcg_summoner_icon + ".png"} 
+                                    alt={"profileIcon"} className="player_img" loading="lazy"/>
+                                    <div className="player_name">
+                                        {selectPlayerData[0].lcg_summoner_nickname}
+                                    </div>
+                                </div>
+                                <div className="head_section head_rank">
+                                    <img src={"/img/rank_" + selectPlayerData[0].lcg_present_tier + ".png"} 
+                                    alt={"present_tier"} className="rank_img" loading="lazy"/>
+                                    {selectPlayerData[0].lcg_present_tier === 'NA' ? 
+                                        <div className="rank_desc">
+                                            UNRANK
+                                        </div>
+                                        :
+                                        <div className="rank_desc">
+                                            {selectPlayerData[0].lcg_present_tier}&nbsp;{selectPlayerData[0].lcg_present_division}
+                                        </div>
+                                    }
+                                </div>
+                            </> : <></>
+                    }
+                    {
+                        !!selectPlayerWinningRate && !!selectPlayerAllKda ? 
+                            <>
+                                <div className="head_data">
+                                    <div className="head_graph">
+                                        <div className="graph_gage">
+                                            <strong>{selectPlayerWinningRate[0].rate}%</strong>
+                                        </div>
+                                        <svg viewBox="0 0 200 200">
+                                            <circle cx="100" cy="100" r="80" fill="none" stroke="#E84057" strokeWidth="30">
+                                            </circle>
+                                            <circle cx="100" cy="100" r="80" fill="none" stroke="#5383E8" strokeWidth="30" 
+                                            strokeDasharray={`${2 * Math.PI * 80 * (parseInt(selectPlayerWinningRate[0].rate) / 100)} ${
+                                                2 * Math.PI * 80 * (Math.trunc((1 - (parseInt(selectPlayerWinningRate[0].rate) / 100)) * 100) / 100)
+                                            }`}
+                                            strokeDashoffset={2 * Math.PI * 80 * 0.25}>
+                                            </circle>
+                                        </svg>
+                                    </div>
+                                    <div className="head_detail">
+                                        <div className="detail_play">
+                                            <div className="count_play">
+                                                {selectPlayerWinningRate[0].lcg_count_play} 게임
+                                            </div>
+                                            <div className="count_wof">
+                                                {selectPlayerWinningRate[0].lcg_count_victory}승&nbsp;{selectPlayerWinningRate[0].lcg_count_defeat}패
+                                            </div>
+                                        </div>
+                                        <div className="detail_kda">
+                                            <div className="calc_kda">
+                                                <Style.LcgKdaCalc $k={selectPlayerAllKda[0].lcg_count_kill} $d={selectPlayerAllKda[0].lcg_count_death} $a={selectPlayerAllKda[0].lcg_count_assist}>
+                                                    {selectPlayerAllKda[0].lcg_count_death !== 0 ? 
+                                                        <span>
+                                                            KDA
+                                                            <span className="kda">
+                                                                {((selectPlayerAllKda[0].lcg_count_kill + selectPlayerAllKda[0].lcg_count_assist) / selectPlayerAllKda[0].lcg_count_death).toFixed(2)}
+                                                            </span>
+                                                        </span>: <span className="kda">Perfect</span>}
+                                                </Style.LcgKdaCalc>
+                                            </div>
+                                            <div className="origin_kda">
+                                                <span>{(selectPlayerAllKda[0].lcg_count_kill / selectPlayerAllKda[0].lcg_count_play).toFixed(1)}</span>/
+                                                <span>{(selectPlayerAllKda[0].lcg_count_death / selectPlayerAllKda[0].lcg_count_play).toFixed(1)}</span>/
+                                                <span>{(selectPlayerAllKda[0].lcg_count_assist / selectPlayerAllKda[0].lcg_count_play).toFixed(1)}</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </> : <></>
+                    }
+                </div>
+                <div className="box_body">
+                    <div className="body_section body_left">
+                        <div className="relative_head">
+                            <div className="head_opponent">
+                                상대
+                            </div>
+                            <div className="head_winningRate">
+                                승률
+                            </div>
+                            <div className="head_matchLine">
+                                포지션
+                            </div>
+                        </div>
+                        {selectPlayerRelative?.map((data, idx) => {
+                            return (
+                                <div className="relative_item" key={"champ_" + idx}>
+                                    <div className="player_opponent">
+                                        {data.lcg_summoner_nickname.split("#")[0]}
+                                    </div>
+                                    <div className="player_winningRate">
+                                        <div className="match_detail">
+                                            {data.lcg_play_count}전&nbsp;/&nbsp;{data.lcg_win_count}승&nbsp;/&nbsp;{data.lcg_fail_count}패
+                                        </div>
+                                        <div className="match_winning">
+                                            {((data.lcg_win_count * 100) / data.lcg_play_count).toFixed(1)}%
+                                        </div>
+                                    </div>
+                                    <div className="player_matchLine">
+                                        {
+                                            data.lcg_match_line === 'TOP' ? <TopIcon />
+                                            :
+                                            data.lcg_match_line === 'JUG' ? <JugIcon />
+                                            :
+                                            data.lcg_match_line === 'MID' ? <MidIcon />
+                                            :
+                                            data.lcg_match_line === 'ADC' ? <AdcIcon />
+                                            :
+                                            <SupIcon />
+                                        }
+                                    </div>
+                                </div>
+                            )
+                        })}
+                    </div>
+                    <div className="body_section body_right">
+                        <div className="champion_head">
+                            <div className="head_champion">
+                                챔피언
+                            </div>
+                            <div className="head_kda">
+                                KDA
+                            </div>
+                            <div className="head_winningRate">
+                                승률
+                            </div>
+                        </div>
+                        {selectPlayerChampion?.map((data, idx) => {
+                            return (
+                                <div className="champion_item" key={"relative_" + idx}>
+                                    <div className="player_champion">
+                                        <img src={imageUrl1 + "champion/" + data.lcg_champion_name + ".png"} 
+                                        alt={"champion"} className="champion_img"  loading="lazy"/>
+                                    </div>
+                                    <div className="player_kda">
+                                        <div className="match_detail">
+                                            <span>{(data.lcg_kill_count / data.lcg_play_count).toFixed(1)}</span>/
+                                            <span>{(data.lcg_death_count / data.lcg_play_count).toFixed(1)}</span>/
+                                            <span>{(data.lcg_assist_count / data.lcg_play_count).toFixed(1)}</span>
+                                        </div>
+                                        <div className="match_kda">
+                                            <Style.LcgKdaCalc $k={data.lcg_kill_count} $d={data.lcg_death_count} $a={data.lcg_assist_count}>
+                                                {data.lcg_death_count !== 0 ? 
+                                                    <span>
+                                                        KDA
+                                                        <span className="kda">
+                                                            {((data.lcg_kill_count + data.lcg_assist_count) / data.lcg_death_count).toFixed(2)}
+                                                        </span>
+                                                    </span>: <span className="kda">Perfect</span>}
+                                            </Style.LcgKdaCalc>
+                                        </div>
+                                    </div>
+                                    <div className="player_winningRate">
+                                        <span>{data.lcg_play_count}전</span>
+                                        <span>{data.lcg_win_count}승</span>
+                                        <span>{data.lcg_fail_count}패</span>
+                                    </div>
+                                </div>
+                            )
+                        })}
+                    </div>
+                </div>
+            </Style.PlayerDataBox>
+        </Style.MatchPlayer>
+    )
+}
+
+export default MatchPlayer;
