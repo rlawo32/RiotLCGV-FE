66f6a1cb41a645a6195e814176e0c1314624551d
rlawo <rlawo32@naver.com>
2025-02-21 01:16:00 +0900

    [RiotLCGV][FE] <SUPABASE> LcgPlayerData_Query_추가_20250221 (1)

--- src/app/queries/getLcgPlayerDataQuery.ts
+++ src/app/queries/getLcgPlayerDataQuery.ts
@@ -5,3 +5,32 @@ export function getLcgPlayerDataQuery(client:TypedSupabaseClient) {
     .from("lcg_player_data")
     .select("lcg_summoner_puuid, lcg_player, lcg_summoner_name, lcg_summoner_nickname")
 }
+
+export function getSelectLcgWinningRateQuery(client:TypedSupabaseClient, puuid:string) {
+  return client.rpc("all_winningrate_rank").eq("lcg_summoner_puuid", puuid)
+}
+
+export function getSelectLcgAllKdaQuery(client:TypedSupabaseClient, puuid:string) {
+  return client
+    .from("lcg_player_statistics")
+    .select("lcg_count_play, lcg_count_kill, lcg_count_death, lcg_count_assist")
+    .eq("lcg_summoner_puuid", puuid)
+}
+
+export function getSelectLcgPlayerDataQuery(client:TypedSupabaseClient, puuid:string) {
+  return client
+    .from("lcg_player_data")
+    .select("lcg_summoner_puuid, lcg_summoner_nickname, lcg_summoner_icon, lcg_present_tier, lcg_rank_win, lcg_present_division, lcg_present_high_tier, lcg_present_high_division, lcg_previous_tier, lcg_previous_division, lcg_previous_high_tier, lcg_previous_high_division, lcg_rank_point")
+    .eq("lcg_summoner_puuid", puuid)
+}
+
+export function getSelectLcgPlayerChampionQuery(client:TypedSupabaseClient, puuid:string) {
+  return client
+    .from("lcg_player_champion")
+    .select("lcg_champion_name, lcg_kill_count, lcg_death_count, lcg_assist_count, lcg_play_count, lcg_win_count, lcg_fail_count")
+    .eq("lcg_puuid", puuid)
+}
+
+export function getSelectLcgPlayerRelativeQuery(client:TypedSupabaseClient, puuid:string) {
+  return client.rpc("player_relative").eq("lcg_person_puuid", puuid)
+}
