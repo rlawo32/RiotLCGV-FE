a51fa6a06f60fab68f294fc94f9dda1da2d33463
rlawo <rlawo32@naver.com>
2025-02-21 23:13:12 +0900

    [RiotLCGV][FE] <View> MatchShuffle_작업_20250221 (2)

--- src/app/views/match_shuffle.style.tsx
+++ src/app/views/match_shuffle.style.tsx
@@ -1,4 +1,382 @@
 import styled from "styled-components";
 
 export const MatchShuffle = styled('div')`
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+    width: 768px;
+    height: 100%;
+    margin: 0 auto;
+    padding: 70px 30px;
+    border-radius: 10px;
+    background-color: rgb(49 49 60 / .7);
+
+    .list_section {
+        position: relative;
+        display: flex;
+        justify-content: space-between;
+        flex-wrap: wrap;
+        .list_wrap {
+            position: relative;
+            display: flex;
+
+            .list_parent {
+                margin: 0 10px;
+
+                .team_camp {
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+                    font-size: 2.2rem;
+                    font-weight: 700;
+                    margin-bottom: 10px;
+                    svg {height:35px; width:35px;}
+                }
+
+                .team_blue {color:#1F85FD;}
+                .team_red {color:#F60C50;}
+
+                .list_child {
+                    position: relative;
+                    display: flex;
+                    align-items: center;
+                    margin: auto;
+                    
+                    .list_select {
+                        position: absolute;
+                        top: 19px;
+                        left: 15px;
+                    }
+
+                    .list_check {
+                        position: absolute;
+                        top: 19px;
+                        right: 15px;
+                    }
+
+                    .list_image {
+                        position: absolute;
+                        top: 35%;
+                        left: 10px;
+                        transform: translateY(-35%);
+                    }
+                }
+            }
+        }
+    }
+    
+    .btn_section {
+        display: flex;
+        justify-content: space-between;
+        width: 300px;
+        margin-top: 20px;
+    }
+`;
+
+export const InputValueStyle = styled('input')`
+    width: 100px;
+    margin: 20px 10px;
+    padding: 14px 0 13px 20px;
+    border: none;
+    border-radius: 15px;
+    background: rgba(42,50,73, .68);
+    color: #ffffff;
+    font-size: 2.2rem;
+    outline: none;
+
+    &::-webkit-inner-spin-button {
+        -webkit-appearance: none;
+        margin: 0;
+    }
+
+    &::placeholder {
+        color: gray;
+        font-size: 17px;
+        opacity: 0.7;
+    }
+`;
+
+export const InputPlayerStyle = styled('input')<{$camp:number}>`
+    width: 300px;
+    margin: 10px 5px;
+    padding: 10px 55px 10px 70px;
+    border: 2px solid ${({$camp}) => $camp === 0 ? "#1F85FD" : "#F60C50"};
+    border-radius: 15px;
+    background: rgb(28 28 31 / 1);
+    color: #ffffff;
+    font-size: 2.2rem;
+    outline: none;
+
+    &::placeholder {
+        color: gray;
+        font-size: 18px;
+        opacity: 0.7;
+    }
+`;
+
+export const BtnStyle= styled('button')`
+    display: block;
+    margin: 10px auto;
+    padding: 10px 30px;
+    border: none;
+    border-radius: 10px;
+    box-shadow: 0 0 40px rgba(42,50,113, .68);
+    background-color: rgb(28 28 31 / 1);
+    color: #ffffff;
+    font-size: 1.8rem;
+    font-weight: 700;
+    cursor: pointer;
+    transition: 0.5s;
+    transition-duration: .3s;
+
+    &:hover {
+        transform: scale(1.1);
+        box-shadow: 0 0 20px rgba(42,50,113, .68);
+        background-position: right center;
+    }
+`;
+
+export const QuickBtnStyle = styled('button')`
+    flex: 1 1 auto;
+    margin: 10px;
+    padding: 10px 15px;
+    text-align: center;
+    transition: 0.5s;
+    background-size: 200% auto;
+    color: #6cacc5;
+    box-shadow: 0 0 40px rgba(42,50,113, .68);
+    border: none;
+    border-radius: 10px;
+    background-image: linear-gradient(to right, rgba(42,50,113) 0%, rgba(42,50,73, .88) 51%, rgba(42,50,113) 100%);
+    cursor: pointer;
+    &:hover {
+        background-position: right center;
+    }
+`;
+
+export const CheckStyle = styled('input')`
+    display: none;
+
+    &:checked {
+
+        + .check-box {
+            border-color:#7B7A8E;
+    
+            &::after{
+                height: 25px;
+                -moz-animation: dothabottomcheck .2s ease 0s forwards;
+                -o-animation: dothabottomcheck .2s ease 0s forwards;
+                -webkit-animation: dothabottomcheck .2s ease 0s forwards;
+                animation: dothabottomcheck .2s ease 0s forwards;
+            }
+            
+            &::before{
+                height: 60px;
+                box-shadow: 0 0 0 3px rgba(42,50,73, .7);
+                -moz-animation: dothatopcheck .4s ease 0s forwards;
+                -o-animation: dothatopcheck .4s ease 0s forwards;
+                -webkit-animation: dothatopcheck .4s ease 0s forwards;
+                animation: dothatopcheck .4s ease 0s forwards;
+            }
+        }
+    }
+
+    @-moz-keyframes dothabottomcheck{
+      0% { height: 0; }
+      100% { height: 24px; }
+    } 
+    @-webkit-keyframes dothabottomcheck{
+      0% { height: 0; }
+      100% { height: 24px; }
+    }
+    @keyframes dothabottomcheck{
+      0% { height: 0; }
+      100% { height: 24px;  }
+    }
+    @keyframes dothatopcheck{
+      0% { height: 0; }
+      50% { height: 0; }
+      100% { height: 50px; }
+    }
+    @-webkit-keyframes dothatopcheck{
+      0% { height: 0; }
+      50% { height: 0; }
+      100% { height: 50px; }
+    }
+    @-moz-keyframes dothatopcheck{
+      0% { height: 0; }
+      50% { height: 0; }
+      100% { height: 50px; }
+    }
+`;
+
+export const LabelStyle = styled('label')`
+    display: inline-block;
+    height: 27px;
+    width: 27px;
+    margin: 3px 0 0 0;
+    background-color: transparent;
+    border: 2px solid #7B7A8E;
+    border-radius: 5px;
+    position: relative;
+    display: inline-block;
+    -moz-box-sizing: border-box;
+    -webkit-box-sizing: border-box;
+    box-sizing: border-box;
+    -moz-transition: border-color ease .2s;
+    -o-transition: border-color ease .2s;
+    -webkit-transition: border-color ease .2s;
+    transition: border-color ease .2s;
+    cursor:pointer;
+
+    transition-property: transform;
+
+    &:hover {
+        transform: scale(1.2);
+
+        + .tooltip {
+            width: 32px;
+            top: -38px;
+            opacity: 1;
+            visibility: visible;
+            pointer-events: auto;
+            text-shadow: 0px -1px 0px rgba(0, 0, 0, 0.1);
+            background: rgba(42,50,113, .9);
+            color: #c97874;
+            z-index: 1;
+
+            &::before {
+                left: 35%;
+            }
+        }
+    }
+
+    &::before, &::after {
+        -moz-box-sizing: border-box;
+        -webkit-box-sizing: border-box;
+        box-sizing: border-box;
+        position: absolute;
+        height: 0;
+        width: 4px;
+        background-color: #7B7A8E;
+        display: inline-block;
+        -moz-transform-origin: left top;
+        -ms-transform-origin: left top;
+        -o-transform-origin: left top;
+        -webkit-transform-origin: left top;
+        transform-origin: left top;
+        border-radius: 5px;
+        content: '';
+        -webkit-transition: opacity ease .5;
+        -moz-transition: opacity ease .5;
+        transition: opacity ease .5;
+    }
+
+    &::before {
+        top: 17px;
+        left: 12px;
+        -moz-transform: rotate(-135deg);
+        -ms-transform: rotate(-135deg);
+        -o-transform: rotate(-135deg);
+        -webkit-transform: rotate(-135deg);
+        transform: rotate(-135deg);
+    }
+
+    &::after {
+        top: 7px;
+        left: 1px;
+        -moz-transform: rotate(-45deg);
+        -ms-transform: rotate(-45deg);
+        -o-transform: rotate(-45deg);
+        -webkit-transform: rotate(-45deg);
+        transform: rotate(-45deg);
+    }
+
+    @-moz-keyframes dothabottomcheck{
+      0% { height: 0; }
+      100% { height: 14px; }
+    } 
+    @-webkit-keyframes dothabottomcheck{
+      0% { height: 0; }
+      100% { height: 14px; }
+    }
+    @keyframes dothabottomcheck{
+      0% { height: 0; }
+      100% { height: 14px;  }
+    }
+    @keyframes dothatopcheck{
+      0% { height: 0; }
+      50% { height: 0; }
+      100% { height: 25px; }
+    }
+    @-webkit-keyframes dothatopcheck{
+      0% { height: 0; }
+      50% { height: 0; }
+      100% { height: 25px; }
+    }
+    @-moz-keyframes dothatopcheck{
+      0% { height: 0; }
+      50% { height: 0; }
+      100% { height: 25px; }
+    }
+`;
+
+export const SelectStyle = styled('select')`    
+    padding: 5px;
+    border: none;
+    border-radius: 10px;
+    background: rgb(68 68 71 / 1);
+    color: #ffffff;
+    font-size: 1.8rem;
+    cursor: pointer;
+    outline: none;
+
+    &:hover {
+        + .tooltip {
+            width: 45px;
+            top: -35px;
+            opacity: 1;
+            visibility: visible;
+            pointer-events: auto;
+            text-shadow: 0px -1px 0px rgba(0, 0, 0, 0.1);
+            background: rgba(42,50,113, .9);
+            color: #c97874;
+            z-index: 1;
+
+            &::before {
+                left: 30%;
+            }
+        }
+    }
+`;
+
+export const ToolTipStyle = styled('div')`
+    position: absolute;
+    top: 0;
+    height: 25px;
+    font-size: 11px;
+    font-weight: bold;
+    background: rgba(42,50,113, .9);
+    color: #c97874;
+    padding: 5px 8px;
+    border-radius: 10px;
+    box-shadow: 0 10px 10px rgba(0, 0, 0, 0.1);
+    opacity: 1;
+    pointer-events: none;
+    transition: all 0.3s cubic-bezier(0.68, -0.55, 0.265, 1.55);
+    z-index: -1;
+
+    &::before {
+        position: absolute;
+        content: "";
+        height: 8px;
+        width: 8px;
+        background: rgba(42,50,113, .9);
+        bottom: -3px;
+        left: 32%;
+        transform: translate(-50%) rotate(45deg);
+        transition: all 0.3s cubic-bezier(0.68, -0.55, 0.265, 1.55);
+    }
 `;
--- src/app/views/match_shuffle.tsx
+++ src/app/views/match_shuffle.tsx
@@ -1,12 +1,317 @@
 'use client'
 
+import TeamBlueIcon from "../icons/TeamBlueIcon";
+import TeamRedIcon from "../icons/TeamRedIcon";
 import * as Style from "./match_shuffle.style";
 
+import { useEffect, useState } from "react";
+
 const MatchShuffle = () => {
+    const [processStep, setProcessStep] = useState<number>(0);
+    const [playerCount, setPlayerCount] = useState<number>(10);
+    const [teamCount, setTeamCount] = useState<number>(2);
+    const [shuffleCount, setShuffleCount] = useState<number>(0);
+
+    const [teams, setTeams] = useState<{id:number, lv:number, nm:string}[][]>([[]]);
+    const [fix, setFix] = useState<{id:number, row:number, cell:number}[]>([]);
+
+    const shuffleTime:number = 3000;
+    const decrease:number = 150;
+
+    const createTeam = () => {
+        const realPlayerCount:number = playerCount;
+        const realTeamCount:number = teamCount;
+        const realComposition:number = realPlayerCount / realTeamCount;
+
+        let temp2DemList:{id:number, lv:number, nm:string}[][] = Array.from({length: realTeamCount}, () => Array.from({length: realComposition}));
+
+        if(realPlayerCount % realTeamCount !== 0) {
+            let tempComposition:number = Math.ceil(realPlayerCount/realTeamCount);
+            let tempPersonnel:number = realPlayerCount;
+            const cellArray:number[] = [];
+            let idx:number = 0;
+            
+            for(let i=1; i<=realTeamCount; i++) {
+                cellArray[idx++] = tempComposition;
+                tempPersonnel -= tempComposition;
+                if((realTeamCount - i) === 2 && tempPersonnel % 2 === 0) {
+                    tempComposition = tempPersonnel / 2;
+                } else if((realTeamCount - i) === 3 && tempPersonnel % 3 === 0) {
+                    tempComposition = tempPersonnel / 3;
+                } else if((realTeamCount - i) === 1) {
+                    tempComposition = tempPersonnel;
+                }
+            }
+
+            temp2DemList = [];
+            temp2DemList = Array.from({length: realTeamCount});
+
+            for(let i=0; i<realTeamCount; i++) {
+                temp2DemList[i] = Array.from({length: cellArray[i]});
+            }
+        }
+
+        for(let i=0; i<temp2DemList.length; i++) {
+            let plus:number = 1;
+            for(let j=0; j<temp2DemList[i].length; j++) {
+                temp2DemList[i][j] = {id:i+plus, lv:5, nm:''};
+                plus += realTeamCount;
+            }
+        }    
+
+        setTeams(temp2DemList);
+    }
+
+    const updateInputData = (data:{index:number; arrNo:number; value:string;}) => {
+        const tempProduceTeam:{id:number, lv:number, nm:string}[][] = teams;
+        const copyTempDataList:{id:number, lv:number, nm:string}[][] = JSON.parse(JSON.stringify(tempProduceTeam));
+        const index = copyTempDataList[data.arrNo].findIndex((item) => item.id === data.index);
+        copyTempDataList[data.arrNo][index].nm = data.value;
+        setTeams(copyTempDataList);
+    }
+
+    const updateSelectData = (data:{index:number; arrNo:number; value:number;}) => {
+        const tempProduceTeam:{id:number, lv:number, nm:string}[][] = teams;
+        const copyTempDataList:{id:number, lv:number, nm:string}[][] = JSON.parse(JSON.stringify(tempProduceTeam));
+        const index = copyTempDataList[data.arrNo].findIndex((item) => item.id === data.index);
+        copyTempDataList[data.arrNo][index].lv = data.value;
+        setTeams(copyTempDataList);
+    }
+
+    const updateCheckData = (data:{checked:boolean; index:number; arrNo:number; value:number;}) => {
+        if(data.checked) {
+            setFix(prev => [...prev, {id:data.index, row:data.arrNo, cell:data.value}]);
+        } else {
+            setFix(fix.filter((el) => el.id !== data.index));
+        }
+    }
+
+    const onActiveSelectBox = ():any[] => {
+        const list:any[] = [];
+        const textBox:string[] = ["E", "D", "C", "B", "A"];
+
+        for(let i=4; i>=0; i--) {
+            list.push(<option key={i} value={i+1}>{textBox[i]}</option>);
+        }
+
+        return list;
+    }
+    
+    const activeRandomData = () => {
+        const tempProduceTeam:{id:number, lv:number, nm:string}[][] = teams;
+        const tempPlayerFix:{id:number, row:number, cell:number}[] = fix;
+        const copyTempDataList:{id:number, lv:number, nm:string}[][] = JSON.parse(JSON.stringify(tempProduceTeam));
+    
+        for(let i=copyTempDataList.length-1; i>=0; i--) { 
+            for(let j=copyTempDataList[i].length-1; j>=0; j--) {
+                let n = Math.floor(Math.random() * (i+1));
+                let m = Math.floor(Math.random() * (j+1));
+                [copyTempDataList[n][j], copyTempDataList[i][m]] = [copyTempDataList[i][m], copyTempDataList[n][j]];
+            }
+        }
+
+        if(tempPlayerFix.length > 0) { 
+            for(let i=0; i<tempPlayerFix.length; i++) {
+                for(let j=0; j<copyTempDataList.length; j++) {
+                    for(let x=0; x<copyTempDataList[j].length; x++) {
+                        if(j === tempPlayerFix[i].row && x === tempPlayerFix[i].cell) {
+                            let tempBox:{id:number, lv:number, nm:string} = copyTempDataList[j][x];
+                            let row:number = -1;
+                            let cell:number = -1;
+                            for(let y=0; y<copyTempDataList.length; y++) {
+                                cell = copyTempDataList[y].findIndex((item) => item.id === tempPlayerFix[i].id);
+                                if(cell !== -1) {
+                                    row = y;
+                                    break;
+                                }
+                            }
+                            copyTempDataList[j][x] = copyTempDataList[row][cell];
+                            copyTempDataList[row][cell] = tempBox;
+                        }
+                    }
+                }
+            }
+        }
+        setTeams(copyTempDataList);
+    }
+
+    const activeBalanceData = () => {
+        const realPersonnel:number = playerCount;
+        const realTeamCount:number = teamCount;
+        const realComposition:number = realPersonnel/realTeamCount;
+        const tempProduceTeam:{id:number, lv:number, nm:string}[][] = teams;
+        const tempPlayerFix:{id:number, row:number, cell:number}[] = fix;
+
+        const copyTempDataList:{id:number, lv:number, nm:string}[][] = JSON.parse(JSON.stringify(tempProduceTeam));
+        const temp1DemList:{id:number, lv:number, nm:string}[] = [];
+        let temp2DemList:{id:number, lv:number, nm:string}[][] = Array.from({length: realTeamCount}, () => Array.from({length: realComposition}));
+
+        for(let i=0; i<copyTempDataList.length; i++) {
+            for(let j=0; j<copyTempDataList[i].length; j++) {
+                temp1DemList.push(copyTempDataList[i][j]);
+            }
+        }
+
+        for(let i=temp1DemList.length-1; i>=0; i--) {
+            let j = Math.floor(Math.random() * (i+1));
+            [temp1DemList[i], temp1DemList[j]] = [temp1DemList[j], temp1DemList[i]];
+        }
+
+        temp1DemList.sort((a, b) => {
+            return b.lv - a.lv;
+        });
+
+        if(realPersonnel % realTeamCount !== 0) {
+            let tempComposition:number = Math.ceil(realPersonnel/realTeamCount);
+            let tempPersonnel:number = realPersonnel;
+            const cellArray:number[] = [];
+            let idx:number = 0;
+
+            for(let i=1; i<=realTeamCount; i++) {
+                cellArray[idx++] = tempComposition;
+                tempPersonnel -= tempComposition;
+                if((realTeamCount - i) === 2 && tempPersonnel % 2 === 0) {
+                    tempComposition = tempPersonnel / 2;
+                } else if((realTeamCount - i) === 3 && tempPersonnel % 3 === 0) {
+                    tempComposition = tempPersonnel / 3;
+                } else if((realTeamCount - i) === 1) {
+                    tempComposition = tempPersonnel;
+                }
+            }
+
+            temp2DemList = [];
+            temp2DemList = Array.from({length: realTeamCount});
+            for(let i=0; i<realTeamCount; i++) {
+                temp2DemList[i] = Array.from({length: cellArray[i]});
+            }
+        }
+
+        let tempComposition:number = Math.ceil(realPersonnel/realTeamCount);
+        let tempCompare:{idx:number, sum:number, len:number}[] = [];
+
+        for(let i=0; i<realTeamCount; i++) {
+            tempCompare[i] = {idx:i, sum:0, len:temp2DemList[i].length};
+        }
+
+        let tmpIdx:number = 0;
+
+        for(let i=0; i<tempComposition; i++) {
+            tempCompare.sort((a, b) => {
+                return a.sum - b.sum;
+            });
+            if(realPersonnel % realTeamCount !== 0) {
+                tempCompare.sort((a, b) => {
+                    return a.len - b.len;
+                }); 
+            }
+            for(let j=0; j<realTeamCount; j++) {
+                if(temp2DemList[tempCompare[j].idx].length-1 >= i) {
+                    temp2DemList[tempCompare[j].idx][i] = temp1DemList[tmpIdx++];
+                }
+            }
+            for(let j=0; j<realTeamCount; j++) {
+                if(temp2DemList[tempCompare[j].idx].length-1 >= i) {
+                    tempCompare[j].sum += temp2DemList[tempCompare[j].idx][i].lv;   
+                }
+            }
+        }
+
+        if(tempPlayerFix.length > 0) { 
+            for(let i=0; i<tempPlayerFix.length; i++) {
+                for(let j=0; j<temp2DemList.length; j++) {
+                    for(let x=0; x<temp2DemList[j].length; x++) {
+                        if(j === tempPlayerFix[i].row && x === tempPlayerFix[i].cell) {
+                            let tempBox:{id:number, lv:number, nm:string} = temp2DemList[j][x];
+                            let row:number = -1;
+                            let cell:number = -1;
+                            for(let y=0; y<temp2DemList.length; y++) {
+                                cell = temp2DemList[y].findIndex((item) => item.id === tempPlayerFix[i].id);
+                                if(cell !== -1) {
+                                    row = y;
+                                    break;
+                                }
+                            }
+                            temp2DemList[j][x] = temp2DemList[row][cell];
+                            temp2DemList[row][cell] = tempBox;
+                        }
+                    }
+                }
+            }
+        }
+        setTeams(temp2DemList);
+    }
+    
+    const onClickRandom = () => {
+        let intervalTime:number = shuffleTime;
+        let interval = setInterval(() => {
+            activeRandomData();    
+            intervalTime -= decrease;
+            if(intervalTime === 0) {
+                clearInterval(interval);
+            }
+        }, decrease);
+    }
+
+    const onClickBalance = () => {
+        let intervalTime:number = shuffleTime;
+        let interval = setInterval(() => {
+            activeBalanceData();    
+            intervalTime -= decrease;
+            if(intervalTime === 0) {
+                clearInterval(interval);
+            }
+        }, decrease);
+    }
+
+    useEffect(() => {
+        createTeam();
+    }, [])
 
     return (
         <Style.MatchShuffle>
-            <strong>개발중</strong>
+            <div className="list_section">
+                {teams.map((parent, idx1) => (
+                    <div key={idx1} className="list_wrap" id={parent.length + "_t"}>
+                        <div className="list_parent">
+                            {idx1 === 0 ? 
+                                <div className="team_camp team_blue">
+                                    <TeamBlueIcon />&nbsp;&nbsp;TeamBlue
+                                </div> 
+                                : 
+                                <div className="team_camp team_red">
+                                    <TeamRedIcon />&nbsp;&nbsp;TeamRed
+                                </div>
+                            }
+                            {parent.map((child, idx2) => (
+                                <div key={idx2} className="list_child">
+                                    <div className="list_select">
+                                        <Style.SelectStyle onChange={(e) => updateSelectData({index:child.id, arrNo:idx1, value:parseInt(e.target.value)})} value={child.lv}>
+                                            {onActiveSelectBox()}
+                                        </Style.SelectStyle>    
+                                        <Style.ToolTipStyle className="tooltip">
+                                            Level
+                                        </Style.ToolTipStyle>
+                                    </div>
+                                    <Style.InputPlayerStyle onChange={(e) => updateInputData({index:child.id, arrNo:idx1, value:e.target.value})} value={child.nm} 
+                                                type="text" id={"input_" + child.id} placeholder="이름 입력" $camp={idx1}/>
+                                    <div className="list_check">
+                                        <Style.CheckStyle onChange={(e) => updateCheckData({checked:e.target.checked, index:child.id, arrNo:idx1, value:idx2})} 
+                                                    checked={fix.some(data => data.id === child.id) ? true : false} type="checkbox" id={"chkbx" + child.id} />
+                                        <Style.LabelStyle htmlFor={"chkbx" + child.id} className="check-box" />
+                                        <Style.ToolTipStyle className="tooltip">
+                                            Fix
+                                        </Style.ToolTipStyle>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                ))}
+            </div>
+            <div className="btn_section">
+                <Style.BtnStyle onClick={() => onClickRandom()}>무작위</Style.BtnStyle>
+                <Style.BtnStyle onClick={() => onClickBalance()}>밸런스</Style.BtnStyle>
+            </div>
         </Style.MatchShuffle>
     )
 }
