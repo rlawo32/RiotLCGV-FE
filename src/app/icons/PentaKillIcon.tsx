f46502a809bde6d1d7ca0b531ad276ddfd16ba84
rlawo <rlawo32@naver.com>
2025-03-28 21:10:04 +0900

    [RiotLCGS][FE] <CSS> 반응형(Mobile)_추가_작업_(multiKill_icon)_20250328 (1)

--- src/app/icons/PentaKillIcon.tsx
+++ src/app/icons/PentaKillIcon.tsx
@@ -4,16 +4,29 @@ import styled from "styled-components";
 
 const IconStyle = styled('div')`
     display: inline-block;
+    @media (max-width: 768px) {
+        width: 10px;
+        height: 10px;
+        padding: 0 0 0 1px;
+        font-size: .8rem;
+    }
+    // mobile_view
+    @media (max-width: 480px) {
+        width: 9px;
+        height: 9px;
+        padding: 0;
+        font-size: .7rem;
+    }
     height: 12px;
     width: 12px;
-    padding: 0 1px 0 0;
+    padding: 0;
     margin-right: 2px;
     border: none;
     border-radius: 3px;
     background-color: #FF8200;
     color: #FFFFFF;
     text-align: center;
-    font-size: 10px;
+    font-size: 1rem;
     font-weight: bold;
 `
 
