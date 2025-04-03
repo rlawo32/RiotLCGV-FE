5f6cf340be42e3a8425e61255db94d59eaafed4a
rlawo <rlawo32@naver.com>
2025-04-04 01:38:18 +0900

    [RiotLCGS][FE] <SUPABASE> MatchLog_Query_추가(List Paging)_20250404

--- src/app/queries/getLcgMatchLogQuery.ts
+++ src/app/queries/getLcgMatchLogQuery.ts
@@ -14,3 +14,17 @@ export function getLcgMatchLogLatestQuery(client: TypedSupabaseClient) {
     .order("lcg_game_id", { ascending: false })
     .limit(1)
 }
+
+export function getLcgMatchLogTotalQuery(client: TypedSupabaseClient) {
+  return client
+    .from("lcg_match_log")
+    .select("lcg_game_id")
+}
+
+export function getLcgMatchLogPagingQuery(client: TypedSupabaseClient, page: number) {
+  return client
+    .from("lcg_match_log")
+    .select("*")
+    .order("lcg_game_id", { ascending: false })
+    .range((page - 1) * 10, ((page - 1) * 10) + 10 - 1)
+}
