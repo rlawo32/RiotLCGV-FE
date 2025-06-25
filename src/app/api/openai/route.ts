c0b963f2639eb9121375f70d24b2c46452a03342
sungjae <92013056+rlawo32@users.noreply.github.com>
2025-06-25 15:45:37 +0900

    [RiotLCGV][FE] <OpenAI> OpenAI_통신API_구축_20250625 (4)

--- src/app/api/openai/route.ts
+++ src/app/api/openai/route.ts
@@ -6,11 +6,31 @@ const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 export async function POST(req: NextRequest) {
   try {
     const body = await req.json();
-    const userMessage = body.message;
+    const prompt = body.prompt;
+    const maxToken = body.maxToken;
+    console.log(body);
+
+    // -- Player Summary --
+    // input  max 300 token
+    // output max 700 token
+
+    // -- Game Summary --
+    // input  max 800 token
+    // ouput  max 1,200 token
 
     const completion = await openai.chat.completions.create({
       model: 'gpt-4o',
-      messages: [{ role: 'user', content: userMessage }],
+      messages: [
+        {
+            role: 'system',
+            content: '너는 게임 분석 전문가야. 간결하고 핵심적으로 설명해.',
+        },
+        { 
+            role: 'user', 
+            content: prompt 
+        }],
+        temperature: 1,
+        max_completion_tokens: maxToken,
     });
 
     return NextResponse.json({ result: completion.choices[0].message.content });
