6a688a387dca3e097dabe88eca94da8b0b1f8b11
rlawo <rlawo32@naver.com>
2025-07-03 00:51:23 +0900

    [RiotLCGV][FE] <SUPABASE> PlayerData_Query_Column_추가(playerHide)_20250703

--- src/app/queries/getLcgPlayerDataQuery.ts
+++ src/app/queries/getLcgPlayerDataQuery.ts
@@ -3,7 +3,7 @@ import { TypedSupabaseClient } from "../supabase"
 export function getLcgPlayerDataQuery(client:TypedSupabaseClient) {
   return client
     .from("lcg_player_data")
-    .select("lcg_summoner_puuid, lcg_player, lcg_summoner_name, lcg_summoner_nickname")
+    .select("lcg_summoner_puuid, lcg_player, lcg_summoner_name, lcg_summoner_nickname, lcg_player_hide")
 }
 
 export function getSelectLcgWinningRateQuery(client:TypedSupabaseClient, puuid:string) {
