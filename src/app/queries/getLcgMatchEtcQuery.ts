8cfcb62167a10c6df6d548b5e6607ab407260375
rlawo <rlawo32@naver.com>
2025-08-10 20:07:11 +0900

    [RiotLCGV][FE] <SUPABASE> MatchEtc_Query_컬럼_추가_작업(lcg_r2_image)_20250810

--- src/app/queries/getLcgMatchEtcQuery.ts
+++ src/app/queries/getLcgMatchEtcQuery.ts
@@ -3,7 +3,7 @@ import { TypedSupabaseClient } from "../supabase"
 export function getLcgMatchEtcQuery(client:TypedSupabaseClient) {
   return client
     .from("lcg_match_etc")
-    .select("lcg_cdn, lcg_lang, lcg_main_ver, lcg_main_image, lcg_sub_image, lcg_update_player, lcg_update_data")
+    .select("lcg_cdn, lcg_lang, lcg_main_ver, lcg_main_image, lcg_sub_image, lcg_r2_image, lcg_update_player, lcg_update_data")
     .order("lcg_update_date", { ascending: false })
     .limit(1)
 }
