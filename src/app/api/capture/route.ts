9999db514f3e4c5ff236470ca97600bd6262a4d0
rlawo <rlawo32@naver.com>
2026-01-04 16:00:19 +0900

    [RiotLCGV][FE] <API> Capture_통신_API_수정_20260104

--- src/app/api/capture/route.ts
+++ src/app/api/capture/route.ts
@@ -2,7 +2,10 @@ import { NextRequest, NextResponse } from 'next/server';
 
 export async function POST(req: NextRequest) {
   try {
-    const captureUrl = 'http://34.127.40.154:8080/send-image';
+    // const captureUrl = 'http://34.127.40.154:8080/send-image'; // GCP
+    // const captureUrl = 'http://158.179.163.174:8080/send-image'; // Oracle
+    const captureUrl = 'https://riotlcgc-be.onrender.com/send-image'; // Render Deploy
+    // const captureUrl = 'http://localhost:8080/send-image'; // Local
     const contentType = req.headers.get('content-type') || '';
 
     const bodyBuffer = Buffer.from(await req.arrayBuffer());
