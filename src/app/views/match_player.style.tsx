a5b718c01977bb3fb4ea6ff81dd71f97e8f48017
sungjae <92013056+rlawo32@users.noreply.github.com>
2026-01-07 17:15:29 +0900

    [RiotLCGV][FE] <CSS> MatchPlayer_History_반응형_작업_20260107 (10)

--- src/app/views/match_player.style.tsx
+++ src/app/views/match_player.style.tsx
@@ -1039,8 +1039,8 @@ export const MatchItem = styled('div')<{$win:string}>`
                 .empty_image {
                     // mobile_view
                     @media (max-width: 768px) {
-                        height: 14px;
-                        width: 14px;
+                        height: 15px;
+                        width: 15px;
                     }
                     height: 22px;
                     width: 22px;
@@ -1052,7 +1052,7 @@ export const MatchItem = styled('div')<{$win:string}>`
                 // mobile_view
                 @media (max-width: 768px) {
                     padding: 0;
-                    margin-left: 1px;
+                    margin: 0;
                 }
                 padding-bottom: 3px;
                 margin-left: 5px;
