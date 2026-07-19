7c099871688e73507eb0f8664ebf3bc8ae45f87e
rlawo <rlawo32@naver.com>
2026-07-19 13:40:50 +0900

    [RiotLCGV][FE] <Tool> UserAgent_Mobile_구분_추가_20260719

---
+++ src/app/component/useIsMobile.ts
@@ -0,0 +1,14 @@
+import { useState, useEffect } from 'react';
+
+export function useIsMobile() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const userAgent = typeof window !== 'undefined' ? navigator.userAgent : '';
+    const mobileRegex = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i;
+    
+    setIsMobile(mobileRegex.test(userAgent));
+  }, []);
+
+  return isMobile;
+}
