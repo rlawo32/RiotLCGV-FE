0f8c05d9c1086830d484fdb7b2723257b124f65b
rlawo <rlawo32@naver.com>
2026-09-02 01:12:37 +0900

    [RiotLCGV][FE] <SUPABASE> DataType_설정_20260902 (41)

--- src/app/queries/getLcgPatchNoteQuery.ts
+++ src/app/queries/getLcgPatchNoteQuery.ts
@@ -6,7 +6,7 @@ export function getLcgPatchNoteVersionsQuery(client:TypedSupabaseClient) {
 
 export function getLcgPatchNoteQuery(client:TypedSupabaseClient, version:string) {
   return client
-    .from("lcg_patch_note")
+    .from("lcg_info_patch")
     .select("lcg_patch_version, lcg_patch_section, lcg_patch_html")
     .eq("lcg_patch_version", version)
     .order("lcg_patch_section", { ascending: true })
